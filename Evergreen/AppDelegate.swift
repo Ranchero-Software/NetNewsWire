6a68db21e2cce76bf3d2cea0336f13ae21da35ca
Brent Simmons <brent@ranchero.com>
2017-05-22 13:00:45 -0700

    Add project and workspace.

---
+++ Evergreen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 5/22/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Evergreen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 5/22/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
