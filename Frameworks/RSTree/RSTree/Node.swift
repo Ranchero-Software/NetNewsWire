b2e3da5cb65f1f640749873ce2ca55a1fcd13574
Brent Simmons <brent@ranchero.com>
2017-05-22 13:29:37 -0700

    Add RSTree framework.

---
+++ Frameworks/RSTree/RSTree/Node.swift
@@ -0,0 +1,112 @@
+//
+//  Node.swift
+//  Rainier
+//
+//  Created by Brent Simmons on 7/21/15.
+//  Copyright © 2015 Ranchero Software, LLC. All rights reserved.
+//
+
+import Foundation
+
+public final class Node: Equatable {
+	
+	public weak var parent: Node?
+	public let representedObject: AnyObject
+	public var canHaveChildNodes = false
+	public var isGroupItem = false
+	public var childNodes: [Node]?
+	
+	public var isRoot: Bool {
+		get {
+			if let _ = parent {
+				return false
+			}
+			return true
+		}
+	}
+	
+	public var numberOfChildNodes: Int {
+		get {
+			return childNodes?.count ?? 0
+		}
+	}
+	
+	public var indexPath: IndexPath {
+		get {
+			if let parent = parent {
+				let parentPath = parent.indexPath
+				if let childIndex = parent.indexOfChild(self) {
+					return parentPath.appending(childIndex)
+				}
+				preconditionFailure("A Node’s parent must contain it as a child.")
+			}
+			return IndexPath(index: 0) //root node
+		}
+	}
+	
+	public var level: Int {
+		get {
+			if let parent = parent {
+				return parent.level + 1
+			}
+			return 0
+		}
+	}
+	
+	public var isLeaf: Bool {
+		get {
+			return numberOfChildNodes < 1
+		}
+	}
+
+	public init(representedObject: AnyObject, parent: Node?) {
+		
+		self.representedObject = representedObject
+		self.parent = parent
+	}
+	
+	public class func genericRootNode() -> Node {
+		
+		let node = Node(representedObject: TopLevelRepresentedObject(), parent: nil)
+		node.canHaveChildNodes = true
+		return node
+	}
+	
+	public func childAtIndex(_ index: Int) -> Node? {
+		
+		guard let childNodes = childNodes else {
+			return nil
+		}
+		if index >= childNodes.count || index < 0 {
+			return nil
+		}
+		return childNodes[index]
+	}
+
+	public func indexOfChild(_ node: Node) -> Int? {
+		
+		return childNodes?.index{ (oneChildNode) -> Bool in
+			oneChildNode === node
+		}
+	}
+	
+	public func childNodeRepresentingObject(_ obj: AnyObject) -> Node? {
+		
+		guard let childNodes = childNodes else {
+			return nil
+		}
+		
+		for oneNode in childNodes {
+			if oneNode.representedObject === obj {
+				return oneNode
+			}
+		}
+		return nil
+	}
+}
+
+
+public func ==(lhs: Node, rhs: Node) -> Bool {
+	
+	return lhs === rhs
+}
---
+++ Frameworks/RSTree/RSTree/TreeController.swift
@@ -0,0 +1,121 @@
+//
+//  TreeController.swift
+//  Rainier
+//
+//  Created by Brent Simmons on 5/29/16.
+//  Copyright © 2016 Ranchero Software, LLC. All rights reserved.
+//
+
+import Foundation
+
+public protocol TreeControllerDelegate: class {
+	
+	func treeController(treeController: TreeController, childNodesFor: Node) -> [Node]?
+}
+
+public typealias NodeVisitBlock = (_ : Node) -> Void
+
+public final class TreeController {
+
+	fileprivate weak var delegate: TreeControllerDelegate?
+	public let rootNode: Node
+
+	public init(delegate: TreeControllerDelegate, rootNode: Node) {
+		
+		self.delegate = delegate
+		self.rootNode = rootNode
+		rebuild()
+	}
+
+	public convenience init(delegate: TreeControllerDelegate) {
+		
+		self.init(delegate: delegate, rootNode: Node.genericRootNode())
+	}
+	
+	@discardableResult
+	public func rebuild() -> Bool {
+
+		// Rebuild and re-sort. Return true if any changes in the entire tree.
+		
+		return rebuildChildNodes(node: rootNode)
+	}
+	
+	public func visitNodes(_ visitBlock: NodeVisitBlock) {
+		
+		visitNode(rootNode, visitBlock)
+	}
+	
+	public func nodeInArrayRepresentingObject(nodes: [Node], representedObject: AnyObject, recurse: Bool = false) -> Node? {
+		
+		for oneNode in nodes {
+
+			if oneNode.representedObject === representedObject {
+				return oneNode
+			}
+
+			if recurse, oneNode.canHaveChildNodes, let childNodes = oneNode.childNodes {
+				if let foundNode = nodeInArrayRepresentingObject(nodes: childNodes, representedObject: representedObject, recurse: recurse) {
+					return foundNode
+				}
+
+			}
+		}
+		return nil
+	}
+
+	public func nodeInTreeRepresentingObject(_ representedObject: AnyObject) -> Node? {
+
+		return nodeInArrayRepresentingObject(nodes: [rootNode], representedObject: representedObject, recurse: true)
+	}
+}
+
+private extension TreeController {
+	
+	func visitNode(_ node: Node, _ visitBlock: NodeVisitBlock) {
+		
+		visitBlock(node)
+		node.childNodes?.forEach{ (oneChildNode) in
+			visitNode(oneChildNode, visitBlock)
+		}
+	}
+	
+	func nodeArraysAreEqual(_ nodeArray1: [Node]?, _ nodeArray2: [Node]?) -> Bool {
+		
+		if nodeArray1 == nil && nodeArray2 == nil {
+			return true
+		}
+		if nodeArray1 != nil && nodeArray2 == nil {
+			return false
+		}
+		if nodeArray1 == nil && nodeArray2 != nil {
+			return false
+		}
+		
+		return nodeArray1! == nodeArray2!
+	}
+	
+	func rebuildChildNodes(node: Node) -> Bool {
+		
+		if !node.canHaveChildNodes {
+			return false
+		}
+		
+		var childNodesDidChange = false
+		
+		let childNodes = delegate?.treeController(treeController: self, childNodesFor: node)
+		
+		childNodesDidChange = !nodeArraysAreEqual(childNodes, node.childNodes)
+		if (childNodesDidChange) {
+			node.childNodes = childNodes
+		}
+		
+		childNodes?.forEach{ (oneChildNode) in
+			if rebuildChildNodes(node: oneChildNode) {
+				childNodesDidChange = true
+			}
+		}
+
+		return childNodesDidChange
+	}
+	
+}
