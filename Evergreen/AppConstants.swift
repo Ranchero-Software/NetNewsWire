85ff386f4b771e9836d3a47d527686e5c0c21ad3
Brent Simmons <brent@ranchero.com>
2017-05-26 13:10:30 -0700

    Add AppConstants.

---
+++ Evergreen/AppConstants.swift
@@ -0,0 +1,27 @@
+//
+//  AppConstants.swift
+//  Rainier
+//
+//  Created by Brent Simmons on 8/30/15.
+//  Copyright © 2015 Ranchero Software, LLC. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+	
+	static let SidebarSelectionDidChange = Notification.Name("SidebarSelectionDidChangeNotification")
+	static let TimelineSelectionDidChange = Notification.Name("TimelineSelectionDidChangeNotification")
+
+	static let AppNavigationKeyPressed = Notification.Name("AppNavigationKeyPressedNotification")
+}
+
+let viewKey = "view"
+let nodeKey = "node"
+let objectsKey = "objects"
+let articleKey = "article"
+
+let articlesKey = "articles"
+let articleStatusKey = "statusKey"
+
+let appNavigationKey = "keyKey"
