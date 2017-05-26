ef9d4edbd1ed44df2c7e60568e1b91b9ed4d6f8a
Brent Simmons <brent@ranchero.com>
2017-05-26 13:25:02 -0700

    Add KeyboardDelegate.

---
+++ Evergreen/MainWindow/KeyboardDelegateProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardDelegateProtocol.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 10/11/16.
+//  Copyright © 2016 Ranchero Software. All rights reserved.
+//
+
+import Cocoa
+
+let keypadEnter: unichar = 3
+
+protocol KeyboardDelegate: class {
+	
+	// Return true if handled.
+	func handleKeydownEvent(_: NSEvent, sender: AnyObject) -> Bool
+}
