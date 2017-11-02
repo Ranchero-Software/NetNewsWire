129f7a5da9f451d223d58c447feee556ff16a3c8
Brent Simmons <brent@ranchero.com>
2017-11-01 20:45:04 -0700

    Create FeedListViewController and FeedListTimelineViewController.

---
+++ Evergreen/FeedList/FeedListTimelineViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FeedListTimelineViewController.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 11/1/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Foundation
---
+++ Evergreen/FeedList/FeedListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FeedListViewController.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 11/1/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Cocoa
+
+final class FeedListViewController: NSViewController {
+
+	
+}
