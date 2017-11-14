5d781d1657f84f21bfeee1054c983a1d02489d3d
Brent Simmons <brent@ranchero.com>
2017-11-13 18:19:25 -0800

    Add method to display contents of file in WebViewWindowController.

--- Frameworks/RSCore/RSCore/AppKit/WebViewWindowController.swift
+++ Frameworks/RSCore/RSCore/AppKit/WebViewWindowController.swift
@@ -24,4 +24,14 @@ public final class WebViewWindowController: NSWindowController {
 
 		window!.title = title
 	}
+
+	public func displayContents(of path: String) {
+
+		// We assume there might be images, style sheets, etc. contained by the folder that the file appears in, so we get read access to the parent folder.
+
+		let fileURL = URL(fileURLWithPath: path)
+		let folderURL = fileURL.deletingLastPathComponent()
+
+		webview.loadFileURL(fileURL, allowingReadAccessTo: folderURL)
+	}
 }
