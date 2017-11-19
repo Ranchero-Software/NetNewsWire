93b8da15dcb5bd9c1751f6244f45ff51d77f3662
Brent Simmons <brent@ranchero.com>
2017-11-19 11:43:31 -0800

    Create PseudoFeed.swift. This will be used for Today, All Unread, Starred, etc.

---
+++ Evergreen/Data/PseudoFeed.swift
@@ -0,0 +1,92 @@
+//
+//  PseudoFeed.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 11/19/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Foundation
+import Data
+import RSCore
+import Account
+
+protocol PseudoFeedDelegate: DisplayNameProvider {
+
+	func fetchUnreadCount(for: Account, callback: (Int) -> Void)
+}
+
+final class PseudoFeed: UnreadCountProvider, DisplayNameProvider {
+
+	private static let fetchCoalescingDelay: TimeInterval = 0.1
+	private var timer: Timer?
+	private var unreadCounts = [Account: Int]()
+	private let delegate: PseudoFeedDelegate
+
+	var nameForDisplay: String {
+		get {
+			return delegate.nameForDisplay
+		}
+	}
+
+	var unreadCount = 0 {
+		didSet {
+			if unreadCount != oldValue {
+				postUnreadCountDidChangeNotification()
+			}
+		}
+	}
+
+	init(delegate: PseudoFeedDelegate) {
+
+		self.delegate = delegate
+	}
+
+	@objc func unreadCountDidChange(_ note: Notification) {
+
+		if let object = note.object, object is Account {
+			startCoalescingTimer()
+		}
+	}
+
+	private func fetchUnreadCount(for account: Account) {
+
+		delegate.fetchUnreadCount(for: account) { (accountUnreadCount) in
+			unreadCounts[account] = accountUnreadCount
+		}
+	}
+
+	private func fetchUnreadCounts() {
+
+		AccountManager.shared.accounts.forEach { self.fetchUnreadCount(for: $0) }
+	}
+
+	private func updateUnreadCount() {
+
+		unreadCount = AccountManager.shared.accounts.reduce(0) { (result, account) -> Int in
+			if let oneUnreadCount = unreadCounts[account] {
+				return result + oneUnreadCount
+			}
+			return result
+		}
+	}
+
+	// MARK: - Timer
+
+	private func stopTimer() {
+
+		if let timer = timer {
+			timer.rs_invalidateIfValid()
+		}
+		timer = nil
+	}
+
+	private func startCoalescingTimer() {
+
+		stopTimer()
+		timer = Timer(timeInterval: PseudoFeed.fetchCoalescingDelay, repeats: false, block: { (_) in
+			self.fetchUnreadCounts()
+			self.stopTimer()
+		})
+	}
+}
