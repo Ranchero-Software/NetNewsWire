1288309995ae0535328c9e756a975bba3e40eb8f
Brent Simmons <brent@ranchero.com>
2017-11-21 13:12:22 -0800

    Make window have a title — this allows people to show labels for toolbar items.

--- Evergreen/MainWindow/MainWindowController.swift
+++ Evergreen/MainWindow/MainWindowController.swift
@@ -29,7 +29,7 @@ class MainWindowController : NSWindowController, NSUserInterfaceValidations {
         
         super.windowDidLoad()
 
-		window?.titleVisibility = .hidden
+//		window?.titleVisibility = .hidden
 		window?.setFrameUsingName(windowAutosaveName, force: true)
 		
 		detailSplitViewItem?.minimumThickness = 384
