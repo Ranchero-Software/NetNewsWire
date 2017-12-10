16b19185806f0c53c143218970cc10b1214cbd70
Brent Simmons <brent@ranchero.com>
2017-12-10 12:31:10 -0800

    Continue work on web services client.

--- Frameworks/RSWeb/RSWeb/WebServices/APICall.swift
+++ Frameworks/RSWeb/RSWeb/WebServices/APICall.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+// Main thread only.
+
+public protocol APICallDelegate {
+
+	func apiCallURLRequest(_ call: APICall) -> URLRequest?
+	func apiCall(_ call: APICall, parseReturnedObjectWith: HTTPResult) -> Any?
+	func apiCall(_ call: APICall, handleErrorWith: HTTPResult)
+	func apiCall(_ call: APICall, performActionWith: HTTPResult)
+}
+
+public final class APICall {
+
+	// Create request. Call server. Create result. Create returned object. Run action.
+
+	public let provider: WebServiceProvider
+	public let url: URL
+	public let credentials: Credentials?
+	public let methodName: String
+	public let identifier: Int
+	private let delegate: APICallDelegate
+	private static var incrementingIdentifier = 0
+
+	init(provider: WebServiceProvider, url: URL, credentials: Credentials?, methodName: String, delegate: APICallDelegate) {
+
+		self.provider = provider
+		self.url = url
+		self.credentials = credentials
+		self.methodName = methodName
+
+		self.identifier = APICall.incrementingIdentifier
+		APICall.incrementingIdentifier += 1
+	}
+}
+
