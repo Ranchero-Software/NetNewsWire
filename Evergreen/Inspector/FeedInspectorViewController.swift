e4beadb84d84d665f37f7d9ae22f3a0090dbd2d5
Brent Simmons <brent@ranchero.com>
2018-01-21 20:35:44 -0800

    Display image, name, feed URL, and home page URL in feed inspector.

--- Evergreen/Inspector/FeedInspectorViewController.swift
+++ Evergreen/Inspector/FeedInspectorViewController.swift
@@ -11,8 +11,17 @@ import Data
 
 final class FeedInspectorViewController: NSViewController, Inspector {
 
+	@IBOutlet var imageView: NSImageView!
+	@IBOutlet var nameTextField: NSTextField!
+	@IBOutlet var homePageURLTextField: NSTextField!
+	@IBOutlet var urlTextField: NSTextField!
+
 	let isFallbackInspector = false
-	var objects: [Any]?
+	var objects: [Any]? {
+		didSet {
+			updateUI()
+		}
+	}
 
 	func canInspect(_ objects: [Any]) -> Bool {
 
@@ -20,6 +29,68 @@ final class FeedInspectorViewController: NSViewController, Inspector {
 	}
 
 	func willEndInspectingObjects() {
+		
+		makeUIEmpty()
+	}
+}
 
+private extension FeedInspectorViewController {
+
+	private var feed: Feed? {
+		guard let objects = objects, objects.count == 1, let feed = objects.first as? Feed else {
+			return nil
+		}
+		return feed
+	}
+
+	func updateUI() {
+
+		view.needsLayout = true
+
+		guard let feed = feed else {
+			makeUIEmpty()
+			return
+		}
+
+		updateImage(feed)
+		updateName(feed)
+		updateHomePageURL(feed)
+		updateFeedURL(feed)
+	}
+
+	func updateImage(_ feed: Feed) {
+
+		if let image = appDelegate.feedIconDownloader.icon(for: feed) {
+			imageView.image = image
+		}
+		else if let image = appDelegate.faviconDownloader.favicon(for: feed) {
+			imageView.image = image
+		}
+		else {
+			imageView.image = nil
+		}
+	}
+
+	func updateName(_ feed: Feed) {
+
+		nameTextField.stringValue = feed.editedName ?? feed.name ?? ""
+	}
+
+	func updateHomePageURL(_ feed: Feed) {
+
+		homePageURLTextField.stringValue = feed.homePageURL ?? ""
+	}
+
+	func updateFeedURL(_ feed: Feed) {
+
+		urlTextField.stringValue = feed.url
+	}
+
+	func makeUIEmpty() {
+
+		imageView.image = nil
+		nameTextField.stringValue = ""
+		homePageURLTextField.stringValue = ""
+		urlTextField.stringValue = ""
 	}
 }
