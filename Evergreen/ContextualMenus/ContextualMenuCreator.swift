199cfcb4ebc3b3c7a6ee584f8eba08e803ce56cc
Brent Simmons <brent@ranchero.com>
2018-01-28 12:13:43 -0800

    Create ContextualMenuCreator.

---
+++ Evergreen/ContextualMenus/ContextualMenuCreator.swift
@@ -0,0 +1,11 @@
+//
+//  ContextualMenuCreator.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 1/28/18.
+//  Copyright © 2018 Ranchero Software. All rights reserved.
+//
+
+import AppKit
+
+
