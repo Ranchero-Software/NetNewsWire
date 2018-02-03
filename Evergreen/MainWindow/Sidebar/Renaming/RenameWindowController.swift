70a3e6cc89ca062b76d162c435a80c4bd61ecfc9
Brent Simmons <brent@ranchero.com>
2018-02-02 23:00:09 -0800

    Wire up outlets for the rename sheet.

--- Evergreen/MainWindow/Sidebar/Renaming/RenameWindowController.swift
+++ Evergreen/MainWindow/Sidebar/Renaming/RenameWindowController.swift
@@ -8,4 +8,19 @@
 
 import AppKit
 
+final class RenameWindowController: NSWindowController {
 
+	@IBOutlet var renamePrompt: NSTextField!
+	@IBOutlet var newTitleTextField: NSTextField!
+	@IBOutlet var renameButton: NSButton!
+	
+	private var originalTitle: String!
+
+	public convenience init(originalTitle: String) {
+
+		self.init(windowNibName: NSNib.Name(rawValue: "RenameSheet"))
+		self.originalTitle = originalTitle
+	}
+
+
+}
