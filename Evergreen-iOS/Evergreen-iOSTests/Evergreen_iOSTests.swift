e3bb14f9a0976a88a821a10f033bad0d538cccef
Brent Simmons <brent@ranchero.com>
2018-02-02 22:10:14 -0800

    Create iOS project and add it to workspace.

---
+++ Evergreen-iOS/Evergreen-iOSTests/Evergreen_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Evergreen_iOSTests.swift
+//  Evergreen-iOSTests
+//
+//  Created by Brent Simmons on 2/2/18.
+//  Copyright © 2018 Ranchero Software, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Evergreen_iOS
+
+class Evergreen_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
