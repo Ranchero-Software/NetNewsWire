920be7b0d17df1c8b2030541751872e50ec63f33
Brent Simmons <brent@ranchero.com>
2018-02-02 21:56:17 -0800

    Start work on the sidebar gear menu. There are commands there now. They don’t all work.

---
+++ Evergreen/MainWindow/Sidebar/Renaming/RenameWindowController.swift
@@ -0,0 +1,9 @@
+//
+//  RenameWindowController.swift
+//  Evergreen
+//
+//  Created by Brent Simmons on 1/28/18.
+//  Copyright © 2018 Ranchero Software. All rights reserved.
+//
+
+import Foundation
