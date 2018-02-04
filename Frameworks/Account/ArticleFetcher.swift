a6db249ab6f2a26c0bd3a53364a8362f2aa8d4d5
Brent Simmons <brent@ranchero.com>
2018-02-04 10:57:41 -0800

    Add ArticleFetcher protocol. Add Feed and Folder extensions for it.

---
+++ Frameworks/Account/ArticleFetcher.swift
@@ -0,0 +1,55 @@
+//
+//  ArticleFetcher.swift
+//  Account
+//
+//  Created by Brent Simmons on 2/4/18.
+//  Copyright © 2018 Ranchero Software, LLC. All rights reserved.
+//
+
+import Foundation
+import Data
+
+public protocol ArticleFetcher {
+
+	func fetchArticles() -> Set<Article>
+	func fetchUnreadArticles() -> Set<Article>
+}
+
+extension Feed: ArticleFetcher {
+
+	public func fetchArticles() -> Set<Article> {
+
+		guard let account = account else {
+			assertionFailure("Expected feed.account, but got nil.")
+			return Set<Article>()
+		}
+		return account.fetchArticles(for: self)
+	}
+
+	public func fetchUnreadArticles() -> Set<Article> {
+
+		guard let account = account else {
+			assertionFailure("Expected feed.account, but got nil.")
+			return Set<Article>()
+		}
+		return account.fetchUnreadArticles(for: self)
+	}
+}
+
+extension Folder: ArticleFetcher {
+
+	public func fetchArticles() -> Set<Article> {
+
+		return fetchUnreadArticles()
+	}
+
+	public func fetchUnreadArticles() -> Set<Article> {
+
+		guard let account = account else {
+			assertionFailure("Expected folder.account, but got nil.")
+			return Set<Article>()
+		}
+
+		return account.fetchArticles(folder: self)
+	}
+}
