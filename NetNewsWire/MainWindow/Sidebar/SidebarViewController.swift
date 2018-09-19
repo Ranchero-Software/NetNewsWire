0a20b26ed57bf998fac166c9af4f198050da1f06
Brent Simmons <brent@ranchero.com>
2018-09-19 13:09:09 -0700

    Remove the internal folder type from the dragged types registration. Since folders can’t be moved inside other folders, we don’t actually accept folder drops. This will have to be revisited once we have multiple accounts, though — you might drag a folder from one account to another.

--- NetNewsWire/MainWindow/Sidebar/SidebarViewController.swift
+++ NetNewsWire/MainWindow/Sidebar/SidebarViewController.swift
@@ -45,7 +45,7 @@ import RSCore
 		outlineView.dataSource = dataSource
 		outlineView.setDraggingSourceOperationMask(.move, forLocal: true)
 		outlineView.setDraggingSourceOperationMask(.copy, forLocal: false)
-		outlineView.registerForDraggedTypes([FeedPasteboardWriter.feedUTIInternalType, FolderPasteboardWriter.folderUTIInternalType, FeedPasteboardWriter.feedUTIType])
+		outlineView.registerForDraggedTypes([FeedPasteboardWriter.feedUTIInternalType, FeedPasteboardWriter.feedUTIType])
 
 		NotificationCenter.default.addObserver(self, selector: #selector(unreadCountDidChange(_:)), name: .UnreadCountDidChange, object: nil)
 		NotificationCenter.default.addObserver(self, selector: #selector(containerChildrenDidChange(_:)), name: .ChildrenDidChange, object: nil)
