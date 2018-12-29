2e159deac8277cb5396f82a1813461e66921ef10
Brent Simmons <brent@ranchero.com>
2018-12-28 21:49:21 -0800

    Add AppConstants.

---
+++ NetNewsWire/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  NetNewsWire
+//
+//  Created by Brent Simmons on 12/28/18.
+//  Copyright © 2018 Ranchero Software. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+	static let privacyPolicyURL = "https://ranchero.com/netnewswire/privacypolicy"
+}
