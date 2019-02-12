4a8634c0e84cfed6a80d2bdf3c38cd44ab3c987d
Brent Simmons <brent@ranchero.com>
2019-02-11 22:35:42 -0800

    Import AppKit instead of Cocoa in the hopes of not getting the Swift Core Data library added (since we’re not using Core Data).

--- NetNewsWire/CrashReporter/CrashReportWindowController.swift
+++ NetNewsWire/CrashReporter/CrashReportWindowController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Ranchero Software. All rights reserved.
 //
 
-import Cocoa
+import AppKit
 
 final class CrashReportWindowController: NSWindowController {
 
--- NetNewsWire/DatabaseWindow/DatabaseWindowController.swift
+++ NetNewsWire/DatabaseWindow/DatabaseWindowController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Ranchero Software. All rights reserved.
 //
 
-import Cocoa
+import AppKit
 
 class DatabaseWindowController: NSWindowController {
 
