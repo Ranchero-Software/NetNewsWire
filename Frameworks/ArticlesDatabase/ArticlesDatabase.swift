44edbc7b0996e55168483a694eda709b741085d6
Brent Simmons <brent@ranchero.com>
2019-02-22 22:17:05 -0800

    Add search index to database. Move create-table statements into ArticlesDatabase since we can do big strings.

--- Frameworks/ArticlesDatabase/ArticlesDatabase.swift
+++ Frameworks/ArticlesDatabase/ArticlesDatabase.swift
@@ -30,10 +30,11 @@ public final class ArticlesDatabase {
 		let queue = RSDatabaseQueue(filepath: databaseFilePath, excludeFromBackup: false)
 		self.articlesTable = ArticlesTable(name: DatabaseTableName.articles, accountID: accountID, queue: queue)
 
-		let createStatementsPath = Bundle(for: type(of: self)).path(forResource: "CreateStatements", ofType: "sql")!
-		let createStatements = try! NSString(contentsOfFile: createStatementsPath, encoding: String.Encoding.utf8.rawValue)
-		queue.createTables(usingStatements: createStatements as String)
+		queue.createTables(usingStatements: ArticlesDatabase.tableCreationStatements)
 		queue.update { (database) in
+			if !self.articlesTable.containsColumn("searchRowID", in: database) {
+				database.executeStatements("ALTER TABLE articles add column searchRowID INTEGER;")
+			}
 			database.executeStatements("DROP TABLE if EXISTS tags;DROP INDEX if EXISTS tags_tagName_index;DROP INDEX if EXISTS articles_feedID_index;DROP INDEX if EXISTS statuses_read_index;")
 		}
 		queue.vacuumIfNeeded()
@@ -98,4 +99,29 @@ public final class ArticlesDatabase {
 	public func markEverywhereAsRead() {
 		articlesTable.markEverywhereAsRead()
 	}
+}
+
+// MARK: - Private
+
+private extension ArticlesDatabase {
+
+	static let tableCreationStatements = """
+	CREATE TABLE if not EXISTS articles (articleID TEXT NOT NULL PRIMARY KEY, feedID TEXT NOT NULL, uniqueID TEXT NOT NULL, title TEXT, contentHTML TEXT, contentText TEXT, url TEXT, externalURL TEXT, summary TEXT, imageURL TEXT, bannerImageURL TEXT, datePublished DATE, dateModified DATE, searchRowID INTEGER);
+
+	CREATE TABLE if not EXISTS statuses (articleID TEXT NOT NULL PRIMARY KEY, read BOOL NOT NULL DEFAULT 0, starred BOOL NOT NULL DEFAULT 0, userDeleted BOOL NOT NULL DEFAULT 0, dateArrived DATE NOT NULL DEFAULT 0);
+
+	CREATE TABLE if not EXISTS authors (authorID TEXT NOT NULL PRIMARY KEY, name TEXT, url TEXT, avatarURL TEXT, emailAddress TEXT);
+	CREATE TABLE if not EXISTS authorsLookup (authorID TEXT NOT NULL, articleID TEXT NOT NULL, PRIMARY KEY(authorID, articleID));
+
+	CREATE TABLE if not EXISTS attachments(attachmentID TEXT NOT NULL PRIMARY KEY, url TEXT NOT NULL, mimeType TEXT, title TEXT, sizeInBytes INTEGER, durationInSeconds INTEGER);
+	CREATE TABLE if not EXISTS attachmentsLookup(attachmentID TEXT NOT NULL, articleID TEXT NOT NULL, PRIMARY KEY(attachmentID, articleID));
+
+	CREATE INDEX if not EXISTS articles_feedID_datePublished_articleID on articles (feedID, datePublished, articleID);
+
+	CREATE INDEX if not EXISTS statuses_starred_index on statuses (starred);
+
+	CREATE VIRTUAL TABLE if not EXISTS search using fts4(title, body);
+
+	CREATE TRIGGER if not EXISTS articles_after_delete_trigger_delete_search_text after delete on articles begin delete from search where rowid = OLD.searchRowID; end;
+	"""
 }
