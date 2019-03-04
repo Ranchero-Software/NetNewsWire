129759ba7fb1b116b5c87b611ee802bd809e85af
Brent Simmons <brent@ranchero.com>
2019-03-03 21:52:55 -0800

    Comment-out the code that runs when timeline view becomes first responder. It fixed a bug, but introduced a new bug.

--- NetNewsWire/MainWindow/Timeline/TimelineTableView.swift
+++ NetNewsWire/MainWindow/Timeline/TimelineTableView.swift
@@ -21,15 +21,15 @@ class TimelineTableView: NSTableView {
 		super.keyDown(with: event)
 	}
 	
-	override func becomeFirstResponder() -> Bool {
-		if super.becomeFirstResponder() {
-			if selectedRow == -1 && numberOfRows > 0 {
-				rs_selectRowAndScrollToVisible(0)
-			}
-			return true
-		}
-		return false
-	}
+//	override func becomeFirstResponder() -> Bool {
+//		if super.becomeFirstResponder() {
+//			if selectedRow == -1 && numberOfRows > 0 {
+//				rs_selectRowAndScrollToVisible(0)
+//			}
+//			return true
+//		}
+//		return false
+//	}
 
 	// MARK: - NSView
 
