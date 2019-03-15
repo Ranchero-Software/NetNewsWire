91897f8ea8ec4ec3d75897d8e5f108ea0379b968
Brent Simmons <brent@ranchero.com>
2019-03-14 22:23:16 -0700

    Import default feeds using OPML. Delete the old plist file.

--- Importers/DefaultFeedsImporter.swift
+++ Importers/DefaultFeedsImporter.swift
@@ -7,53 +7,26 @@
 //
 
 import Foundation
-import Articles
 import Account
 import RSCore
 
-typealias DiskFeedDictionary = [String: Any]
-
 struct DefaultFeedsImporter {
 	
-	static func importIfNeeded(_ firstRun: Bool, account: Account) {
-		
-		if shouldImportDefaultFeeds(firstRun) {
-			appDelegate.logDebugMessage("Importing default feeds.")
-			FeedsImporter.importFeeds(defaultFeeds(), account: account)
+	static func importIfNeeded(_ isFirstRun: Bool, account: Account) {
+		guard shouldImportDefaultFeeds(isFirstRun) else {
+			return
 		}
+
+		appDelegate.logDebugMessage("Importing default feeds.")
+		let defaultFeedsURL = Bundle.main.url(forResource: "DefaultFeeds", withExtension: "opml")!
+		try! OPMLImporter.parseAndImport(fileURL: defaultFeedsURL, account: AccountManager.shared.localAccount)
 	}
-	
-	private static func defaultFeeds() -> [DiskFeedDictionary] {
-		
-		let f = Bundle.main.path(forResource: "DefaultFeeds", ofType: "plist")!
-		return NSArray(contentsOfFile: f)! as! [DiskFeedDictionary]
-	}
-	
+
 	private static func shouldImportDefaultFeeds(_ isFirstRun: Bool) -> Bool {
-		
 		if !isFirstRun || AccountManager.shared.anyAccountHasAtLeastOneFeed() {
 			return false
 		}
 		return true
-	}
-}
-
-struct FeedsImporter {
-	
-	static func importFeeds(_ feedDictionaries: [DiskFeedDictionary], account: Account) {
-		
-		let feedsToImport = feeds(with: feedDictionaries, account: account)
-		
-		BatchUpdate.shared.perform {
-			feedsToImport.forEach{ account.addFeed($0) }
-		}
-		account.structureDidChange()
-	}
-	
-	private static func feeds(with feedDictionaries: [DiskFeedDictionary], account: Account) -> Set<Feed> {
-
-		let feedArray = feedDictionaries.compactMap { Feed(account: account, dictionary: $0) }
-		return Set(feedArray)
 	}
 }
 
