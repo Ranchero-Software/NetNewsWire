1a448770aef9fbed5889099f3e4b1f55e98493c4
Brent Simmons <brent@ranchero.com>
2019-03-24 12:33:43 -0700

    Continue progress on accounts UI in Preferences.

--- NetNewsWire/Preferences/Accounts/AccountsPreferencesViewController.swift
+++ NetNewsWire/Preferences/Accounts/AccountsPreferencesViewController.swift
@@ -7,10 +7,61 @@
 //
 
 import AppKit
+import Account
 
 final class AccountsPreferencesViewController: NSViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
+	private var sortedAccounts = [Account]()
+
+	override func viewWillAppear() {
+		updateSortedAccounts()
+	}
+}
+
+// MARK: - NSTableViewDataSource
+
+extension AccountsPreferencesViewController: NSTableViewDataSource {
+
+	func numberOfRows(in tableView: NSTableView) -> Int {
+		return sortedAccounts.count
+	}
+
+	func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+		return sortedAccounts[row]
+	}
+}
+
+// MARK: - NSTableViewDelegate
+
+extension AccountsPreferencesViewController: NSTableViewDelegate {
+
+	private static let cellIdentifier = NSUserInterfaceItemIdentifier(rawValue: "AccountCell")
+
+	func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+
+		func configure(_ cell: AccountsTableCellView) {
+		}
+
+		if let cell = tableView.makeView(withIdentifier: AccountsPreferencesViewController.cellIdentifier, owner: nil) as? AccountsTableCellView {
+			configure(cell)
+			return cell
+		}
+
+		let cell = AccountsTableCellView()
+		cell.identifier = AccountsPreferencesViewController.cellIdentifier
+		configure(cell)
+		return cell
+	}
+
+	func tableViewSelectionDidChange(_ notification: Notification) {
+	}
+}
+
+// MARK: - Private
+
+private extension AccountsPreferencesViewController {
+
+	func updateSortedAccounts() {
+		sortedAccounts = AccountManager.shared.sortedAccounts
 	}
 }
---
+++ NetNewsWire/Preferences/Accounts/AccountsTableCellView.swift
@@ -0,0 +1,14 @@
+//
+//  AccountsTableCellView.swift
+//  NetNewsWire
+//
+//  Created by Brent Simmons on 3/23/19.
+//  Copyright © 2019 Ranchero Software. All rights reserved.
+//
+
+import AppKit
+
+final class AccountsTableCellView: NSTableCellView {
+
+	
+}
