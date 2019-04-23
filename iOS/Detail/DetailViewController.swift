bf1f02d0841d03a38782d33a31d60d68d3364cd2
Maurice Parker <mo@vincode.io>
2019-04-23 09:52:49 -0500

    Change the detail view back button when the timeline changes.

--- iOS/Detail/DetailViewController.swift
+++ iOS/Detail/DetailViewController.swift
@@ -80,6 +80,13 @@ class DetailViewController: UIViewController {
 		let starImage = article.status.starred ? AppAssets.starClosedImage : AppAssets.starOpenImage
 		starBarButtonItem.image = starImage
 		
+		if let timelineName = navState?.timelineName {
+			if navigationController?.navigationItem.backBarButtonItem?.title != timelineName {
+				let backItem = UIBarButtonItem(title: timelineName, style: .plain, target: nil, action: nil)
+				navigationController?.navigationItem.backBarButtonItem = backItem
+			}
+		}
+		
 	}
 	
 	func reloadHTML() {
