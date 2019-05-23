13c0f3a09c65c6ad763e724eef5ad8d54ed76bae
Phil Viso <phil.viso@gmail.com>
2019-05-22 21:17:50 -0500

    UI improvements for add feedbin account

--- iOS/Settings/AddAccountViewController.swift
+++ iOS/Settings/AddAccountViewController.swift
@@ -24,14 +24,14 @@ class AddAccountViewController: UITableViewController, AddAccountDismissDelegate
 	}
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let storyboard = UIStoryboard.settings
 		switch indexPath.row {
 		case 0:
-			let addViewController = UIStoryboard.settings.instantiateViewController(withIdentifier: "AddLocalAccountViewController") as! AddLocalAccountViewController
+			let addViewController = storyboard.instantiateViewController(withIdentifier: "AddLocalAccountViewController") as! AddLocalAccountViewController
 			addViewController.delegate = self
 			navigationController?.pushViewController(addViewController, animated: true)
 		case 1:
-			let navController = UIStoryboard.settings.instantiateViewController(withIdentifier: "FeedbinAccountNavigationViewController") as! UINavigationController
-			let addViewController = navController.topViewController as! FeedbinAccountViewController
+			let addViewController = storyboard.instantiateViewController(withIdentifier: "FeedbinAccountViewController") as! FeedbinAccountViewController
 			addViewController.delegate = self
 			navigationController?.pushViewController(addViewController, animated: true)
 		default:
