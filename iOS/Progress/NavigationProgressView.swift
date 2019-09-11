b99483f32de9049b0281e2a6f3a7cef4f7ad33cc
Maurice Parker <mo@vincode.io>
2019-09-11 11:59:28 -0500

    Change progress bar to match bar tint color

--- iOS/Progress/NavigationProgressView.swift
+++ iOS/Progress/NavigationProgressView.swift
@@ -24,7 +24,7 @@ public final class NavigationProgressView: UIView {
 	
 	internal let bar = UIView()
 	
-	@objc public dynamic var progressTintColor: UIColor? = AppAssets.netNewsWireBlueColor {
+	@objc public dynamic var progressTintColor: UIColor? = AppAssets.barTintColor {
 		didSet {
 			bar.backgroundColor = progressTintColor
 		}
