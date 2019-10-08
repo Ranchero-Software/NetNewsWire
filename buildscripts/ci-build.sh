3aede1a23e18a69026630bf12c61ad75702b4626
Maurice Parker <mo@vincode.io>
2019-10-08 17:18:13 -0500

    Add Github Actions configuation

---
+++ buildscripts/ci-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -v
+set -e
+
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dev.cer.enc -d -a -out buildscripts/certs/dev.cer
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dev.p12.enc -d -a -out buildscripts/certs/dev.p12
+
+security create-keychain -p github-actions github-build.keychain
+security import buildscripts/certs/apple.cer -k ~/Library/Keychains/github-build.keychain -A
+security import buildscripts/certs/dev.cer -k ~/Library/Keychains/github-build.keychain -A
+security import buildscripts/certs/dev.p12 -k ~/Library/Keychains/github-build.keychain -P $KEY_SECRET -A
+security set-key-partition-list -S apple-tool:,apple: -s -k github-actions github-build.keychain
+security default-keychain -s github-build.keychain
+
+rm -f ./buildscripts/certs/dev.cer
+rm -f ./buildscripts/certs/dev.p12
+
+xcodebuild -scheme 'NetNewsWire' -configuration Release -allowProvisioningUpdates -showBuildTimingSummary
+
+security delete-keychain github-build.keychain
