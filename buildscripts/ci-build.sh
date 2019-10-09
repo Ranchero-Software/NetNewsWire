d80b83eaf39f11e6ab4051eda8af3b45c33b5678
Maurice Parker <mo@vincode.io>
2019-10-09 16:36:06 -0500

    Updates to do manual provisioning

--- buildscripts/ci-build.sh
+++ buildscripts/ci-build.sh
@@ -2,9 +2,12 @@
 set -v
 set -e
 
+# Unencrypt our provisioning profile, certificate, and private key
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/profile/NetNewsWire.provisionprofile.enc -d -a -out buildscripts/profile/NetNewsWire.provisionprofile
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dev.cer.enc -d -a -out buildscripts/certs/dev.cer
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dev.p12.enc -d -a -out buildscripts/certs/dev.p12
 
+# Put the certificates and private key in the Keychain, set ACL permissions, and make default
 security create-keychain -p github-actions github-build.keychain
 security import buildscripts/certs/apple.cer -k ~/Library/Keychains/github-build.keychain -A
 security import buildscripts/certs/dev.cer -k ~/Library/Keychains/github-build.keychain -A
@@ -12,9 +15,18 @@ security import buildscripts/certs/dev.p12 -k ~/Library/Keychains/github-build.k
 security set-key-partition-list -S apple-tool:,apple: -s -k github-actions github-build.keychain
 security default-keychain -s github-build.keychain
 
-rm -f ./buildscripts/certs/dev.cer
-rm -f ./buildscripts/certs/dev.p12
+# Copy the provisioning profile
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+cp buildscripts/profile/NetNewsWire.provisionprofile ~/Library/MobileDevice/Provisioning\ Profiles/
 
-xcodebuild -scheme 'NetNewsWire' -configuration Debug -allowProvisioningUpdates -showBuildTimingSummary
+# Delete the decrypted files
+rm -f buildscripts/profile/NetNewsWire.provisionprofile
+rm -f buildscripts/certs/dev.cer
+rm -f buildscripts/certs/dev.p12
 
+# Do the build
+xcodebuild -scheme 'NetNewsWire' -configuration Release -showBuildTimingSummary
+
+# Delete the keychain and the provisioningi profile
 security delete-keychain github-build.keychain
+rm -f ~/Library/MobileDevice/Provisioning\ Profiles/NetNewsWire.provisionprofile
