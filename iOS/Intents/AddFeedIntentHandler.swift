c49867cfe32ba2cd65f4a0000be2f34ccd5136d5
Maurice Parker <mo@vincode.io>
2019-10-18 18:31:00 -0500

    Stub out Intents Extension

---
+++ iOS/Intents/AddFeedIntentHandler.swift
@@ -0,0 +1,22 @@
+//
+//  AddFeedIntentHandler.swift
+//  NetNewsWire
+//
+//  Created by Maurice Parker on 10/18/19.
+//  Copyright © 2019 Ranchero Software. All rights reserved.
+//
+
+import Intents
+
+public class AddFeedIntentHandler: NSObject, AddFeedIntentHandling {
+	
+	public func handle(intent: AddFeedIntent, completion: @escaping (AddFeedIntentResponse) -> Void) {
+		
+	}
+	
+	public func resolveUrl(for intent: AddFeedIntent, with completion: @escaping (INURLResolutionResult) -> Void) {
+		
+	}
+	
+	
+}
