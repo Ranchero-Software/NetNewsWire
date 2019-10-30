2d70a6c1a53406ef64566acdf5c0af25ef8629e6
Maurice Parker <mo@vincode.io>
2019-10-30 13:51:32 -0500

    Prevent insertion point bug.  Issue ##1204

--- iOS/Add/AddFolderViewController.swift
+++ iOS/Add/AddFolderViewController.swift
@@ -31,7 +31,6 @@ class AddFolderViewController: UITableViewController, AddContainerViewController
 		accounts = AccountManager.shared.sortedActiveAccounts
 		
 		nameTextField.delegate = self
-		nameTextField.becomeFirstResponder()
 		
 		accountLabel.text = (accounts[0] as DisplayNameProvider).nameForDisplay
 		
