2fead1f87d50b6040feae06f0ba2b0ffe75b7e20
Maurice Parker <mo@vincode.io>
2019-10-29 20:48:07 -0500

    Update image darkness algorithm to use an average instead of checking individual pixels

--- iOS/UIKit Extensions/UIImage-Extensions.swift
+++ iOS/UIKit Extensions/UIImage-Extensions.swift
@@ -15,27 +15,23 @@ extension CGImage {
 		guard let ptr = CFDataGetBytePtr(imageData) else { return false }
 		
 		let length = CFDataGetLength(imageData)
-		var visiblePixels = 0
-		var darkPixels = 0
+		var pixelCount = 0
+		var totalLuminance = 0.0
 		
 		for i in stride(from: 0, to: length, by: 4) {
 			
 			let r = ptr[i]
 			let g = ptr[i + 1]
 			let b = ptr[i + 2]
-			let a = ptr[i + 3]
 			let luminance = (0.299 * Double(r) + 0.587 * Double(g) + 0.114 * Double(b))
 			
-			if Double(a) > 0.0 {
-				visiblePixels += 1
-				if luminance < 50 {
-					darkPixels += 1
-				}
-			}
+			totalLuminance += luminance
+			pixelCount += 1
 			
 		}
 		
-		return Double(darkPixels) / Double(visiblePixels) > 0.4
+		let avgLuminance = totalLuminance / Double(pixelCount)
+		return avgLuminance < 37.5
 	}
 	
 }
