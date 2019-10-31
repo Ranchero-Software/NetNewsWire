5bcb5a982f697d85d12d08448f0f9069a3397f2d
Maurice Parker <mo@vincode.io>
2019-10-31 13:38:38 -0500

    Cache home pages with no icon between launches

--- iOS/AppDelegate.swift
+++ iOS/AppDelegate.swift
@@ -200,12 +200,13 @@ private extension AppDelegate {
 		let faviconsFolderURL = tempDir.appendingPathComponent("Favicons")
 		let imagesFolderURL = tempDir.appendingPathComponent("Images")
 		let homePageToIconURL = tempDir.appendingPathComponent("HomePageToIconURLCache.plist")
-
+		let homePagesWithNoIconURL = tempDir.appendingPathComponent("HomePagesWithNoIconURLCache.plist")
+		
 		// If the image disk cache hasn't been flushed for 3 days and the network is available, delete it
 		if let flushDate = AppDefaults.lastImageCacheFlushDate, flushDate.addingTimeInterval(3600*24*3) < Date() {
 			if let reachability = try? Reachability(hostname: "apple.com") {
 				if reachability.connection != .unavailable {
-					for tempItem in [faviconsFolderURL, imagesFolderURL, homePageToIconURL] {
+					for tempItem in [faviconsFolderURL, imagesFolderURL, homePageToIconURL, homePagesWithNoIconURL] {
 						do {
 							os_log(.info, log: self.log, "Removing cache file: %@", tempItem.absoluteString)
 							try FileManager.default.removeItem(at: tempItem)
