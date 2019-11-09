8a69657b54b516f3fbcd85dff061ee1457530428
Maurice Parker <mo@vincode.io>
2019-11-09 17:37:25 -0600

    Convert all image locations to be absolute.  Issues #954 & 1280

--- Shared/Article Rendering/main.js
+++ Shared/Article Rendering/main.js
@@ -14,6 +14,13 @@ function stripStyles() {
 	document.getElementsByTagName("body")[0].querySelectorAll("[style]").forEach(element => element.removeAttribute("style"));
 }
 
+// Convert all image locations to be absolute
+function convertImgSrc() {
+	document.querySelectorAll("img").forEach(element => {
+		element.src = new URL(element.src, document.baseURI).href;
+	});
+}
+
 function error() {
 	document.body.innerHTML = "error";
 }
@@ -26,5 +33,7 @@ function render(data) {
 	
 	wrapFrames()
 	stripStyles()
+	convertImgSrc()
+	
 	postRenderProcessing()
 }
