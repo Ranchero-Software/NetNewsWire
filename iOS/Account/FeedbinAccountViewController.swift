fea48e7ab9ab72e0cc65ccac5c77f364c5192162
Maurice Parker <mo@vincode.io>
2019-11-22 15:11:15 -0600

    Increase inspector header padding.   Issue #1324

--- iOS/Account/FeedbinAccountViewController.swift
+++ iOS/Account/FeedbinAccountViewController.swift
@@ -44,7 +44,7 @@ class FeedbinAccountViewController: UITableViewController {
 	}
 
 	override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-		return section == 0 ? 64.0 : super.tableView(tableView, heightForHeaderInSection: section)
+		return section == 0 ? ImageHeaderView.rowHeight : super.tableView(tableView, heightForHeaderInSection: section)
 	}
 	
 	override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
