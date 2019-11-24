e8395c8bfc6edae6c187341234f590bf10191003
Maurice Parker <mo@vincode.io>
2019-11-24 11:25:42 -0600

    Remove "v" from version number.  Issue #1339

--- iOS/Settings/SettingsViewController.swift
+++ iOS/Settings/SettingsViewController.swift
@@ -53,7 +53,7 @@ class SettingsViewController: UITableViewController {
 		let buildLabel = NonIntrinsicLabel(frame: CGRect(x: 20.0, y: 0.0, width: 0.0, height: 0.0))
 		buildLabel.font = UIFont.systemFont(ofSize: 11.0)
 		buildLabel.textColor = UIColor.gray
-		buildLabel.text = "\(Bundle.main.appName) v \(Bundle.main.versionNumber) (Build \(Bundle.main.buildNumber))"
+		buildLabel.text = "\(Bundle.main.appName) \(Bundle.main.versionNumber) (Build \(Bundle.main.buildNumber))"
 		buildLabel.sizeToFit()
 		buildLabel.translatesAutoresizingMaskIntoConstraints = false
 		
