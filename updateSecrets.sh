a4f6adbd852abe9467abd31f7ab0dd30343f5c04
Jonathan Bennett <jonathan@jbennett.me>
2019-11-27 14:06:32 -0500

    vendor the gyb dependency

--- updateSecrets.sh
+++ updateSecrets.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ ! $(type -P "gyb") ]; then
-  echo "gyb not installed. Install via: brew install nshipster/formulae/gyb"
-  exit 1
-fi
-
 find . -name '*.gyb' |
   while read file; do
     echo "running ${file%.gyb}";
-    gyb --line-directive '' -o "${file%.gyb}" "$file";
+    ./Vendor/gyb --line-directive '' -o "${file%.gyb}" "$file";
   done
