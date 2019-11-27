69ebecaf40f76241c682f2783ed6bd3c81f2eb49
Jonathan Bennett <jonathan@jbennett.me>
2019-11-27 11:40:24 -0500

    display instructions if gyb is missing

--- updateSecrets.sh
+++ updateSecrets.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-find . -name '*.gyb' |                                               \
-  while read file; do                                              \
-    echo "running ${file%.gyb}"; \
-    gyb --line-directive '' -o "${file%.gyb}" "$file"; \
+if [ ! $(type -P "gyb") ]; then
+  echo "gyb not installed. Install via: brew install nshipster/formulae/gyb"
+  exit 1
+fi
+
+find . -name '*.gyb' |
+  while read file; do
+    echo "running ${file%.gyb}";
+    gyb --line-directive '' -o "${file%.gyb}" "$file";
   done
