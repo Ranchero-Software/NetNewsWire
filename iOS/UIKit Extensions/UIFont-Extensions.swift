d222a9f6174e501de2f5a7b689c5d4e89ee76040
Maurice Parker <mo@vincode.io>
2020-02-24 08:42:19 -0800

    Change to not force unwrap the descriptor.  Issue #1818

--- iOS/UIKit Extensions/UIFont-Extensions.swift
+++ iOS/UIKit Extensions/UIFont-Extensions.swift
@@ -11,8 +11,11 @@ import UIKit
 extension UIFont {
 	
 	func withTraits(traits:UIFontDescriptor.SymbolicTraits) -> UIFont {
-		let descriptor = fontDescriptor.withSymbolicTraits(traits)
-		return UIFont(descriptor: descriptor!, size: 0) //size 0 means keep the size as it is
+		if let descriptor = fontDescriptor.withSymbolicTraits(traits) {
+			return UIFont(descriptor: descriptor, size: 0) //size 0 means keep the size as it is
+		} else {
+			return self
+		}
 	}
 	
 	func bold() -> UIFont {
