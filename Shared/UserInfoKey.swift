66054ca7353b1df49eddefafd7cfa5fd2cbd05aa
Maurice Parker <mo@vincode.io>
2020-03-01 15:06:23 -0800

    Deleted unused keys.

--- Shared/UserInfoKey.swift
+++ Shared/UserInfoKey.swift
@@ -8,18 +8,10 @@
 
 import Foundation
 
-typealias UserInfoDictionary = [AnyHashable: Any]
-
 struct UserInfoKey {
 
-	static let view = "view"
-	static let article = "article"
-	static let articles = "articles"
-	static let navigationKeyPressed = "navigationKeyPressed"
-	static let objects = "objects"
 	static let webFeed = "webFeed"
 	static let url = "url"
-	static let author = "author"
 	static let articlePath = "articlePath"
 	static let feedIdentifier = "feedIdentifier"
 	
