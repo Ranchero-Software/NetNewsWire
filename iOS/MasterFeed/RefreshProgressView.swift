115405f1f0763780290e4b86106318bea10e5cc3
Michele Volpato <git@volparto.nl>
2020-03-02 22:25:57 +0100

    Fix progress bar width, add contraints to progress label

--- iOS/MasterFeed/RefreshProgressView.swift
+++ iOS/MasterFeed/RefreshProgressView.swift
@@ -13,12 +13,11 @@ class RefreshProgressView: UIView {
 	
 	@IBOutlet weak var progressView: UIProgressView!
 	@IBOutlet weak var label: UILabel!
-	private lazy var progressWidthConstraint = progressView.widthAnchor.constraint(equalToConstant: 100.0)
 	
 	override func awakeFromNib() {
 		NotificationCenter.default.addObserver(self, selector: #selector(progressDidChange(_:)), name: .AccountRefreshProgressDidChange, object: nil)
 		NotificationCenter.default.addObserver(self, selector: #selector(contentSizeCategoryDidChange(_:)), name: UIContentSizeCategory.didChangeNotification, object: nil)
-
+		
 		if !AccountManager.shared.combinedRefreshProgress.isComplete {
 			progressChanged()
 		} else {
@@ -27,11 +26,11 @@ class RefreshProgressView: UIView {
 		
 		scheduleUpdateRefreshLabel()
 	}
-
+	
 	override func didMoveToSuperview() {
 		progressChanged()
 	}
-
+	
 	func updateRefreshLabel() {
 		if let accountLastArticleFetchEndTime = AccountManager.shared.lastArticleFetchEndTime {
 			
@@ -51,19 +50,19 @@ class RefreshProgressView: UIView {
 		} else {
 			label.text = ""
 		}
-
+		
 	}
 	
 	@objc func progressDidChange(_ note: Notification) {
 		progressChanged()
 	}
-
+	
 	@objc func contentSizeCategoryDidChange(_ note: Notification) {
 		// This hack is probably necessary because custom views in the toolbar don't get
 		// notifications that the content size changed.
 		label.font = UIFont.preferredFont(forTextStyle: .footnote)
 	}
-
+	
 	deinit {
 		NotificationCenter.default.removeObserver(self)
 	}
@@ -73,12 +72,12 @@ class RefreshProgressView: UIView {
 // MARK: Private
 
 private extension RefreshProgressView {
-
+	
 	func progressChanged() {
 		// Layout may crash if not in the view hierarchy.
 		// https://github.com/Ranchero-Software/NetNewsWire/issues/1764
 		let isInViewHierarchy = self.superview != nil
-
+		
 		let progress = AccountManager.shared.combinedRefreshProgress
 		
 		if progress.isComplete {
@@ -86,23 +85,22 @@ private extension RefreshProgressView {
 				progressView.setProgress(1, animated: true)
 			}
 			DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-				self.updateRefreshLabel()
-				self.label.isHidden = false
-				self.progressView.isHidden = true
-				self.progressWidthConstraint.isActive = false
-				if isInViewHierarchy {
-					self.progressView.setProgress(0, animated: true)
+				// Check that there are no pending downloads.
+				if (AccountManager.shared.combinedRefreshProgress.isComplete) {
+					self.updateRefreshLabel()
+					self.label.isHidden = false
+					self.progressView.isHidden = true
+					if self.superview != nil {
+						self.progressView.setProgress(0, animated: true)
+					}
 				}
 			}
 		} else {
 			label.isHidden = true
 			progressView.isHidden = false
-			progressWidthConstraint.isActive = true
 			if isInViewHierarchy {
-				progressView.setNeedsLayout()
-				progressView.layoutIfNeeded()
 				let percent = Float(progress.numberCompleted) / Float(progress.numberOfTasks)
-
+				
 				// Don't let the progress bar go backwards unless we need to go back more than 25%
 				if percent > progressView.progress || progressView.progress - percent > 0.25 {
 					progressView.setProgress(percent, animated: true)
