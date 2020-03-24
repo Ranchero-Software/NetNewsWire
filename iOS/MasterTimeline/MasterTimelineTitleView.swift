bd75df7294d0e3a5f96e81890391e2576c5609d7
Maurice Parker <mo@vincode.io>
2020-03-24 16:42:46 -0500

    Add pointer interaction for timeline header button.  Issue #1943

--- iOS/MasterTimeline/MasterTimelineTitleView.swift
+++ iOS/MasterTimeline/MasterTimelineTitleView.swift
@@ -13,5 +13,44 @@ class MasterTimelineTitleView: UIView {
 	@IBOutlet weak var iconView: IconView!
 	@IBOutlet weak var label: UILabel!
 	@IBOutlet weak var unreadCountView: MasterTimelineUnreadCountView!
+
+	@available(iOS 13.4, *)
+	private lazy var pointerInteraction: UIPointerInteraction = {
+		UIPointerInteraction(delegate: self)
+	}()
+	
+	func buttonize() {
+		heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+		accessibilityTraits = .button
+		if #available(iOS 13.4, *) {
+			addInteraction(pointerInteraction)
+		}
+	}
+	
+	func debuttonize() {
+		accessibilityTraits.remove(.button)
+		if #available(iOS 13.4, *) {
+			removeInteraction(pointerInteraction)
+		}
+	}
+	
+}
+
+extension MasterTimelineTitleView: UIPointerInteractionDelegate {
+	
+	@available(iOS 13.4, *)
+	func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
+		
+		let params = UIPreviewParameters()
+		var rect = self.bounds
+		rect.origin.x = rect.origin.x - 10
+		rect.size.width = rect.width + 20
+		let path = UIBezierPath(roundedRect: rect, cornerRadius: 10.0)
+		params.visiblePath = path
+		
+		let preview = UITargetedPreview(view: self, parameters: params)
+
+		return UIPointerStyle(effect: .automatic(preview), shape: .path(path))
+	}
 	
 }
