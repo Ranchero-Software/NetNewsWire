7ceaa305e70dce9fcf07e55cdb82085615c02cd1
Maurice Parker <mo@vincode.io>
2020-06-27 11:22:01 -0500

    Initial multiplatform build

---
+++ Multiplatform/Shared/NetNewsWire_MultiplatformApp.swift
@@ -0,0 +1,18 @@
+//
+//  NetNewsWire_MultiplatformApp.swift
+//  Shared
+//
+//  Created by Maurice Parker on 6/27/20.
+//  Copyright © 2020 Ranchero Software. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct NetNewsWire_MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
