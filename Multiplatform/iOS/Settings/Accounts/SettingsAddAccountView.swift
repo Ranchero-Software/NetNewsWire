5aa329bf1cead9f97247221afa4c6ad02d79b948
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2020-07-07 22:45:12 +0530

    Adds Account settings and New Local account addition

---
+++ Multiplatform/iOS/Settings/Accounts/SettingsAddAccountView.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsAddAccountView.swift
+//  Multiplatform iOS
+//
+//  Created by Rizwan on 07/07/20.
+//  Copyright © 2020 Ranchero Software. All rights reserved.
+//
+
+import SwiftUI
+import Account
+
+struct SettingsAddAccountView: View {
+	@State private var isAddPresented = false
+	@State private var selectedAccountType: AccountType = .onMyMac
+
+	var body: some View {
+		List {
+			Button(action: {
+				self.selectedAccountType = AccountType.onMyMac
+				self.isAddPresented = true
+			}) {
+				SettingsAccountLabelView(
+					accountImage: AppAssets.image(for: .onMyMac),
+					accountLabel: Account.defaultLocalAccountName
+				)
+			}
+			Button(action: {
+				self.selectedAccountType = AccountType.feedbin
+				self.isAddPresented = true
+			}) {
+				SettingsAccountLabelView(
+					accountImage: AppAssets.image(for: .feedbin),
+					accountLabel: "Feedbin"
+				)
+			}
+		}
+		.listStyle(InsetGroupedListStyle())
+		.sheet(isPresented: $isAddPresented) {
+			if selectedAccountType == .onMyMac {
+				SettingsLocalAccountView()
+			}
+			if selectedAccountType == .feedbin {
+				//SettingsFeedbinAccountView(viewModel: SettingsFeedbinAccountView.ViewModel())
+			}
+		}
+		.navigationBarTitle(Text("Add Account"), displayMode: .inline)
+	}
+}
+
+struct SettingsAddAccountView_Previews: PreviewProvider {
+	static var previews: some View {
+		SettingsAddAccountView()
+	}
+}
