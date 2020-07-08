af4849642ce7bb643bf71540f37d878d5b03fdb2
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-07-08 21:39:39 +0800

    Adds multi-select to macOS sidebar
    This hacks around property observers to get to a model that almost works.

--- Multiplatform/Shared/Sidebar/SidebarView.swift
+++ Multiplatform/Shared/Sidebar/SidebarView.swift
@@ -12,54 +12,74 @@ import Account
 struct SidebarView: View {
 	
 	// I had to comment out SceneStorage because it blows up if used on macOS
-//	@SceneStorage("expandedContainers") private var expandedContainerData = Data()
+	//	@SceneStorage("expandedContainers") private var expandedContainerData = Data()
 	@StateObject private var expandedContainers = SidebarExpandedContainers()
 	@EnvironmentObject private var sidebarModel: SidebarModel
 	
+	@StateObject private var sidebarSelectionModel = SidebarSelectionModel()
+	
+	@ViewBuilder
 	var body: some View {
-		List() {
-			ForEach(sidebarModel.sidebarItems) { sidebarItem in
-				if let containerID = sidebarItem.containerID {
-					DisclosureGroup(isExpanded: $expandedContainers[containerID]) {
-						ForEach(sidebarItem.children) { sidebarItem in
-							if let containerID = sidebarItem.containerID {
-								DisclosureGroup(isExpanded: $expandedContainers[containerID]) {
-									ForEach(sidebarItem.children) { sidebarItem in
-										ZStack {
-											SidebarItemView(sidebarItem: sidebarItem)
-											NavigationLink(destination: (TimelineContainerView(feed: sidebarItem.feed))) {
-												EmptyView()
-											}.buttonStyle(PlainButtonStyle())
-										}
-									}
-								} label: {
+		#if os(macOS)
+		List(selection: $sidebarSelectionModel.selectedSidebarItems) {
+			containedList
+		}
+		#else
+		List {
+			containedList
+		}
+		#endif
+		//		.onAppear {
+		//			expandedContainers.data = expandedContainerData
+		//		}
+		//		.onReceive(expandedContainers.objectDidChange) {
+		//			expandedContainerData = expandedContainers.data
+		//		}
+	}
+	
+	var containedList: some View {
+		ForEach(sidebarModel.sidebarItems) { sidebarItem in
+			if let containerID = sidebarItem.containerID {
+				DisclosureGroup(isExpanded: $expandedContainers[containerID]) {
+					ForEach(sidebarItem.children) { sidebarItem in
+						if let containerID = sidebarItem.containerID {
+							DisclosureGroup(isExpanded: $expandedContainers[containerID]) {
+								ForEach(sidebarItem.children) { sidebarItem in
 									ZStack {
 										SidebarItemView(sidebarItem: sidebarItem)
-										NavigationLink(destination: (TimelineContainerView(feed: sidebarItem.feed))) {
+										NavigationLink(destination: (TimelineContainerView(feed: sidebarItem.feed)),
+													   tag: sidebarItem.feed!.feedID!,
+													   selection: $sidebarSelectionModel.selectedSidebarItem) {
 											EmptyView()
 										}.buttonStyle(PlainButtonStyle())
 									}
 								}
-							} else {
+							} label: {
 								ZStack {
 									SidebarItemView(sidebarItem: sidebarItem)
-									NavigationLink(destination: (TimelineContainerView(feed: sidebarItem.feed))) {
+									NavigationLink(destination: (TimelineContainerView(feed: sidebarItem.feed)),
+												   tag: sidebarItem.feed!.feedID!,
+												   selection: $sidebarSelectionModel.selectedSidebarItem) {
 										EmptyView()
 									}.buttonStyle(PlainButtonStyle())
 								}
 							}
+						} else {
+							ZStack {
+								SidebarItemView(sidebarItem: sidebarItem)
+								NavigationLink(destination: (TimelineContainerView(feed: sidebarItem.feed)),
+											   tag: sidebarItem.feed!.feedID!,
+											   selection: $sidebarSelectionModel.selectedSidebarItem) {
+									EmptyView()
+								}.buttonStyle(PlainButtonStyle())
+							}
 						}
-					} label: {
-						SidebarItemView(sidebarItem: sidebarItem)
 					}
+				} label: {
+					SidebarItemView(sidebarItem: sidebarItem)
 				}
 			}
 		}
-//		.onAppear {
-//			expandedContainers.data = expandedContainerData
-//		}
-//		.onReceive(expandedContainers.objectDidChange) {
-//			expandedContainerData = expandedContainers.data
-//		}
 	}
+	
 }
