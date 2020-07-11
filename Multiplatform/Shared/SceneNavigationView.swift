20d989f1b8ce1d51ebacff1f303822e0279d1b1e
Maurice Parker <mo@vincode.io>
2020-07-11 09:48:34 -0500

    Revert "Widget Work in Progress"

--- Multiplatform/Shared/SceneNavigationView.swift
+++ Multiplatform/Shared/SceneNavigationView.swift
@@ -17,7 +17,6 @@ struct SceneNavigationView: View {
 	
 	#if os(iOS)
 	@Environment(\.horizontalSizeClass) private var horizontalSizeClass
-	@Environment(\.scenePhase) private var scenePhase
 	#endif
 	
 	var body: some View {
@@ -64,13 +63,6 @@ struct SceneNavigationView: View {
 		}
 		.onChange(of: sheetToShow) { value in
 			value != .none ? (showSheet = true) : (showSheet = false)
-		}
-		.onChange(of: scenePhase) { newPhase in
-			if newPhase == .background {
-				#if os(iOS)
-				WidgetDataEncoder.encodeWidgetData()
-				#endif
-			}
 		}
 		.toolbar {
 			
