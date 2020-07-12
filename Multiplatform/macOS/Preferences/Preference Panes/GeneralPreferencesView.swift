235da781dbb05df4a20db3a2fbc4403680984afd
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-07-12 21:47:29 +0800

    adds (old version) to other NNW entries

--- Multiplatform/macOS/Preferences/Preference Panes/GeneralPreferencesView.swift
+++ Multiplatform/macOS/Preferences/Preference Panes/GeneralPreferencesView.swift
@@ -13,31 +13,37 @@ struct GeneralPreferencesView: View {
 	@ObservedObject var preferences: MacPreferencesModel
 	
 	var body: some View {
-			Form {
-				Picker("Refresh feeds",
-					   selection: $defaults.interval,
-					   content: {
-						ForEach(RefreshInterval.allCases, content: { interval in
-							Text(interval.description())
-								.tag(interval.rawValue)
-						})
-					   })
-					.frame(width: 300, alignment: .center)
-				
-				Picker("Default RSS reader", selection: $preferences.readerSelection, content: {
-					ForEach(0..<preferences.rssReaders.count, content: { index in
+		Form {
+			Picker("Refresh feeds",
+				   selection: $defaults.interval,
+				   content: {
+					ForEach(RefreshInterval.allCases, content: { interval in
+						Text(interval.description())
+							.tag(interval.rawValue)
+					})
+				   })
+			
+			Picker("Default RSS reader", selection: $preferences.readerSelection, content: {
+				ForEach(0..<preferences.rssReaders.count, content: { index in
+					if index > 0 && preferences.rssReaders[index].nameMinusAppSuffix.contains("NetNewsWire") {
+						Text(preferences.rssReaders[index].nameMinusAppSuffix.appending(" (old version)"))
+
+					} else {
 						Text(preferences.rssReaders[index].nameMinusAppSuffix)
 							.tag(index)
-					})
+					}
+					
+					
 				})
-				
-				
-				Toggle("Open webpages in background in browser", isOn: $defaults.openInBrowserInBackground)
-				
-				Toggle("Hide Unread Count in Dock", isOn: $defaults.hideDockUnreadCount)
-			}
-			.frame(width: 400, alignment: .center)
-			.lineLimit(2)
+			})
+			
+			
+			Toggle("Open webpages in background in browser", isOn: $defaults.openInBrowserInBackground)
+			
+			Toggle("Hide Unread Count in Dock", isOn: $defaults.hideDockUnreadCount)
+		}
+		.frame(width: 400, alignment: .center)
+		.lineLimit(2)
 	}
 	
 }
