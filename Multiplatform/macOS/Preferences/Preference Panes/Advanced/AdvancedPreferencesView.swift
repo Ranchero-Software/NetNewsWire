310cfaf66bdc0e199a60a0369ced6daee7685591
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-07-16 07:02:55 +0800

    Tidies up AdvancedPreferences

--- Multiplatform/macOS/Preferences/Preference Panes/Advanced/AdvancedPreferencesView.swift
+++ Multiplatform/macOS/Preferences/Preference Panes/Advanced/AdvancedPreferencesView.swift
@@ -12,36 +12,30 @@ struct AdvancedPreferencesView: View {
 	@EnvironmentObject private var preferences: AppDefaults
     
     var body: some View {
-        
             Form {
                 Toggle("Check for app updates automatically", isOn: $preferences.checkForUpdatesAutomatically)
-                
                 Toggle("Download Test Builds", isOn: $preferences.downloadTestBuilds)
-				
 				Text("If you’re not sure, don't enable test builds. Test builds may have bugs, which may include crashing bugs and data loss.")
 					.foregroundColor(.secondary)
-					.lineLimit(3)
-					.padding(.bottom, 8)
-                
                 HStack {
                     Spacer()
 					Button("Check for Updates") {
 						appDelegate.softwareUpdater.checkForUpdates()
 					}
                     Spacer()
-                }.padding(.bottom, 8)
-                
-                
+                }
                 Toggle("Send Crash Logs Automatically", isOn: $preferences.sendCrashLogs)
-                
+				Divider()
                 HStack {
                     Spacer()
                     Button("Privacy Policy", action: {
 						NSWorkspace.shared.open(URL(string: "https://ranchero.com/netnewswire/privacypolicy")!)
 					})
                     Spacer()
-                }.padding(.top, 12)
-			}.frame(width: 400, alignment: .center)
+                }
+			}
+			.frame(width: 400, alignment: .center)
+			.lineLimit(3)
     }
     
 }
