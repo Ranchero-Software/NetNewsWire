8881ec71f71e5931dd5d46de5bb472ab822592a1
Maurice Parker <mo@vincode.io>
2020-07-30 05:06:10 -0500

    Changed Secrets to be a Swift Package

---
+++ Secrets/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Secrets",
+    products: [
+        .library(
+            name: "Secrets",
+            targets: ["Secrets"]),
+    ],
+    dependencies: [
+		.package(url: "https://github.com/OAuthSwift/OAuthSwift.git", .upToNextMajor(from: "2.1.2")),
+    ],
+    targets: [
+        .target(
+            name: "Secrets",
+            dependencies: [],
+			exclude: ["Secrets.swift.gyb"]),
+        .testTarget(
+            name: "SecretsTests",
+            dependencies: ["Secrets"]),
+    ]
+)
---
+++ Secrets/README.md
@@ -0,0 +1,3 @@
+# Secrets
+
+A description of this package.
