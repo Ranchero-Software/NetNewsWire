8dbe5691e3c6efe45701527e7f567f445f55ff4a
Maurice Parker <mo@vincode.io>
2020-10-20 13:45:28 -0500

    Update the user interface style on the main thread.

--- iOS/SceneDelegate.swift
+++ iOS/SceneDelegate.swift
@@ -111,13 +111,15 @@ private extension SceneDelegate {
 	}
 	
 	func updateUserInterfaceStyle() {
-		switch AppDefaults.userInterfaceColorPalette {
-		case .automatic:
-			window!.overrideUserInterfaceStyle = .unspecified
-		case .light:
-			window!.overrideUserInterfaceStyle = .light
-		case .dark:
-			window!.overrideUserInterfaceStyle = .dark
+		DispatchQueue.main.async {
+			switch AppDefaults.userInterfaceColorPalette {
+			case .automatic:
+				self.window?.overrideUserInterfaceStyle = .unspecified
+			case .light:
+				self.window?.overrideUserInterfaceStyle = .light
+			case .dark:
+				self.window?.overrideUserInterfaceStyle = .dark
+			}
 		}
 	}
 	
