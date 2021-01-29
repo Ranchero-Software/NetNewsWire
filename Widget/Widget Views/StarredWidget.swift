4414ea6c06aef76877ab05e3c7aa9d4927ae7e7d
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2021-01-29 22:34:31 +0800

    Text position on widget
    Text position on widgets are now in a fixed position.

--- Widget/Widget Views/StarredWidget.swift
+++ Widget/Widget Views/StarredWidget.swift
@@ -23,34 +23,53 @@ struct StarredWidgetView : View {
 		}
 		else {
 			GeometryReader { metrics in
-				HStack(alignment: .top, spacing: 4) {
-					VStack(alignment: .leading, spacing: -4) {
+				HStack {
+					VStack {
 						starredImage
+							.padding(.vertical, 12)
+							.padding(.leading, 8)
 						Spacer()
-						Text(L10n.localizedCount(entry.widgetData.currentStarredCount)).bold().font(.callout).minimumScaleFactor(0.5).lineLimit(1)
-						Text(L10n.starred.lowercased()).bold().font(Font.system(.footnote).lowercaseSmallCaps()).minimumScaleFactor(0.5).lineLimit(1)
-					}
-					.frame(width: metrics.size.width * 0.15)
-					.padding(.trailing, 4)
 					
-					VStack(alignment:.leading, spacing: 0) {
-						ForEach(0..<maxCount(), content: { i in
-							if i != 0 {
-								Divider()
-								ArticleItemView(article: entry.widgetData.starredArticles[i],
-												deepLink: WidgetDeepLink.starredArticle(id: entry.widgetData.starredArticles[i].id).url)
-									.padding(.top, 8)
-									.padding(.bottom, 4)
-							} else {
-								ArticleItemView(article: entry.widgetData.starredArticles[i],
-												deepLink: WidgetDeepLink.starredArticle(id: entry.widgetData.starredArticles[i].id).url)
-									.padding(.bottom, 4)
-							}
-							
-						})
+					}
+				}
+				.frame(width: metrics.size.width * 0.15)
+				
+				Spacer()
+				
+				VStack(alignment:.leading, spacing: 0) {
+					ForEach(0..<maxCount(), content: { i in
+						if i != 0 {
+							Divider()
+							ArticleItemView(article: entry.widgetData.starredArticles[i],
+											deepLink: WidgetDeepLink.starredArticle(id: entry.widgetData.starredArticles[i].id).url)
+								.padding(.top, 8)
+								.padding(.bottom, 4)
+						} else {
+							ArticleItemView(article: entry.widgetData.starredArticles[i],
+											deepLink: WidgetDeepLink.starredArticle(id: entry.widgetData.starredArticles[i].id).url)
+								.padding(.bottom, 4)
+						}
+					})
+					Spacer()
+				}
+				.padding(.leading, metrics.size.width * 0.175)
+				.padding([.bottom, .trailing])
+				.padding(.top, 12)
+				.overlay(
+					 VStack {
 						Spacer()
-					}.padding(.leading, 4)
-				}.padding()
+						HStack {
+							Spacer()
+							Text(L10n.starredCount(entry.widgetData.currentStarredCount - maxCount()))
+								.font(.caption2)
+								.bold()
+								.foregroundColor(.secondary)
+						}
+					}
+					.padding(.horizontal)
+					.padding(.bottom, 6)
+				)
+			
 			}.widgetURL(WidgetDeepLink.starred.url)
 			
 		}
