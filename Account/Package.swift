6d0ac71e21e73fbb3798ac02db443fb444340b50
Brent Simmons <brent@ranchero.com>
2021-05-23 13:22:53 -0700

    Use RSParser 2.0.2 and higher — make sure we have latest bug fixes.

--- Account/Package.swift
+++ Account/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
 		.package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMajor(from: "1.0.0")),
 		.package(url: "https://github.com/Ranchero-Software/RSDatabase.git", .upToNextMajor(from: "1.0.0")),
-		.package(url: "https://github.com/Ranchero-Software/RSParser.git", .upToNextMajor(from: "2.0.0")),
+		.package(url: "https://github.com/Ranchero-Software/RSParser.git", .upToNextMajor(from: "2.0.2")),
 		.package(url: "https://github.com/Ranchero-Software/RSWeb.git", .upToNextMajor(from: "1.0.0")),
 		.package(url: "../Articles", .upToNextMajor(from: "1.0.0")),
 		.package(url: "../ArticlesDatabase", .upToNextMajor(from: "1.0.0")),
