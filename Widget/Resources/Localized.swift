087aa18563ee44e3210c441741b4fe49add0044f
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2022-04-13 06:48:50 +0800

    Widget follows the uppercase naming convention

--- Widget/Resources/Localized.swift
+++ Widget/Resources/Localized.swift
@@ -12,53 +12,53 @@ import Foundation
 internal enum L10n {
   /// Plural format key: "%#@localized_count@"
   internal static func localizedCount(_ p1: Int) -> String {
-    return L10n.tr("Localizable", "LocalizedCount", p1)
+    return L10n.tr("Localizable", "LOCALIZED_COUNT", p1)
   }
   /// Your smart feeds, summarized.
-  internal static let smartFeedSummaryWidgetDescription = L10n.tr("Localizable", "SmartFeedSummary_Widget_Description")
+  internal static let smartFeedSummaryWidgetDescription = L10n.tr("Localizable", "SMARTFEEDSUMMARY_WIDGET_DESCRIPTION")
   /// Your Smart Feed Summary
-  internal static let smartFeedSummaryWidgetTitle = L10n.tr("Localizable", "SmartFeedSummary_Widget_Title")
+  internal static let smartFeedSummaryWidgetTitle = L10n.tr("Localizable", "SMARTFEEDSUMMARY_WIDGET_TITLE")
   /// Starred
-  internal static let starred = L10n.tr("Localizable", "Starred")
+  internal static let starred = L10n.tr("Localizable", "STARRED")
   /// A sneak peek at your starred articles.
-  internal static let starredWidgetDescription = L10n.tr("Localizable", "Starred_Widget_Description")
+  internal static let starredWidgetDescription = L10n.tr("Localizable", "STARRED_WIDGET_DESCRIPTION")
   /// When you mark articles as Starred, they'll appear here.
-  internal static let starredWidgetNoItems = L10n.tr("Localizable", "Starred_Widget_NoItems")
+  internal static let starredWidgetNoItems = L10n.tr("Localizable", "STARRED_WIDGET_NOITEMS")
   /// Starred
-  internal static let starredWidgetNoItemsTitle = L10n.tr("Localizable", "Starred_Widget_NoItemsTitle")
+  internal static let starredWidgetNoItemsTitle = L10n.tr("Localizable", "STARRED_WIDGET_NOITEMSTITLE")
   /// Your Starred Articles
-  internal static let starredWidgetTitle = L10n.tr("Localizable", "Starred_Widget_Title")
+  internal static let starredWidgetTitle = L10n.tr("Localizable", "STARRED_WIDGET_TITLE")
   /// Plural format key: "%#@starred_count@"
   internal static func starredCount(_ p1: Int) -> String {
-    return L10n.tr("Localizable", "StarredCount", p1)
+    return L10n.tr("Localizable", "STARRED_COUNT", p1)
   }
   /// Today  
-  internal static let today = L10n.tr("Localizable", "Today")
+  internal static let today = L10n.tr("Localizable", "TODAY")
   /// A sneak peek at recently published unread articles.
-  internal static let todayWidgetDescription = L10n.tr("Localizable", "Today_Widget_Description")
+  internal static let todayWidgetDescription = L10n.tr("Localizable", "TODAY_WIDGET_DESCRIPTION")
   /// There are no recent unread articles left to read.
-  internal static let todayWidgetNoItems = L10n.tr("Localizable", "Today_Widget_NoItems")
+  internal static let todayWidgetNoItems = L10n.tr("Localizable", "TODAY_WIDGET_NOITEMS")
   /// Today
-  internal static let todayWidgetNoItemsTitle = L10n.tr("Localizable", "Today_Widget_NoItemsTitle")
+  internal static let todayWidgetNoItemsTitle = L10n.tr("Localizable", "TODAY_WIDGET_NOITEMSTITLE")
   /// Your Today Articles
-  internal static let todayWidgetTitle = L10n.tr("Localizable", "Today_Widget_Title")
+  internal static let todayWidgetTitle = L10n.tr("Localizable", "TODAY_WIDGET_TITLE")
   /// Plural format key: "%#@today_count@"
   internal static func todayCount(_ p1: Int) -> String {
-    return L10n.tr("Localizable", "TodayCount", p1)
+    return L10n.tr("Localizable", "TODAY_COUNT", p1)
   }
   /// Unread
-  internal static let unread = L10n.tr("Localizable", "Unread")
+  internal static let unread = L10n.tr("Localizable", "UNREAD")
   /// A sneak peek at your unread articles.
-  internal static let unreadWidgetDescription = L10n.tr("Localizable", "Unread_Widget_Description")
+  internal static let unreadWidgetDescription = L10n.tr("Localizable", "UNREAD_WIDGET_DESCRIPTION")
   /// There are no unread articles left to read.
-  internal static let unreadWidgetNoItems = L10n.tr("Localizable", "Unread_Widget_NoItems")
+  internal static let unreadWidgetNoItems = L10n.tr("Localizable", "UNREAD_WIDGET_NOITEMS")
   /// Unread
-  internal static let unreadWidgetNoItemsTitle = L10n.tr("Localizable", "Unread_Widget_NoItemsTitle")
+  internal static let unreadWidgetNoItemsTitle = L10n.tr("Localizable", "UNREAD_WIDGET_NOITEMSTITLE")
   /// Your Unread Articles
-  internal static let unreadWidgetTitle = L10n.tr("Localizable", "Unread_Widget_Title")
+  internal static let unreadWidgetTitle = L10n.tr("Localizable", "UNREAD_WIDGET_TITLE")
   /// Plural format key: "%#@unread_count@"
   internal static func unreadCount(_ p1: Int) -> String {
-    return L10n.tr("Localizable", "UnreadCount", p1)
+    return L10n.tr("Localizable", "UNREAD_COUNT", p1)
   }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
