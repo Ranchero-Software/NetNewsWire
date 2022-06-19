a89746df6bf586dcf465cdd135d0c93c1c8c6f31
Ethan Wong <e1hanw0ng@gmail.com>
2022-06-19 11:57:10 +0800

    use .package(path:) for local package dependencies to resolve Xcode 14 errors.

--- SyncDatabase/Package.swift
+++ SyncDatabase/Package.swift
@@ -1,6 +1,21 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+var dependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/Ranchero-Software/RSDatabase.git", .upToNextMajor(from: "1.0.0")),
+]
+
+#if swift(>=5.6)
+dependencies.append(contentsOf: [
+    .package(path: "../Articles"),
+])
+#else
+dependencies.append(contentsOf: [
+    .package(url: "../Articles", .upToNextMajor(from: "1.0.0")),
+])
+#endif
+
 let package = Package(
     name: "SyncDatabase",
 	platforms: [.macOS(SupportedPlatform.MacOSVersion.v10_15), .iOS(SupportedPlatform.IOSVersion.v13)],
@@ -10,11 +25,7 @@ let package = Package(
 			type: .dynamic,
             targets: ["SyncDatabase"]),
     ],
-    dependencies: [
-		.package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMajor(from: "1.0.0")),
-		.package(url: "https://github.com/Ranchero-Software/RSDatabase.git", .upToNextMajor(from: "1.0.0")),
-		.package(url: "../Articles", .upToNextMajor(from: "1.0.0")),
-    ],
+    dependencies: dependencies,
     targets: [
         .target(
             name: "SyncDatabase",
