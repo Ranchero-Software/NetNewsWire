d78d61ad5eb2442b8e66cc7b913c0aef38113ff6
Maurice Parker <mo@vincode.io>
2022-11-12 15:30:40 -0600

    Fetch all changes for the account zone before processing to avoid problems with things getting out of order due to switching queues

--- Account/Sources/Account/CloudKit/CloudKitAccountDelegate.swift
+++ Account/Sources/Account/CloudKit/CloudKitAccountDelegate.swift
@@ -487,7 +487,7 @@ private extension CloudKitAccountDelegate {
 		
 		refreshProgress.isIndeterminate = true
 		refreshProgress.addToNumberOfTasksAndRemaining(3)
-		accountZone.fetchChangesInZone() { result in
+		accountZone.fetchChangesInZone(incremental: false) { result in
 			self.refreshProgress.completeTask()
 
 			let webFeeds = account.flattenedWebFeeds()
@@ -534,7 +534,7 @@ private extension CloudKitAccountDelegate {
 			completion(.failure(error))
 		}
 		
-		accountZone.fetchChangesInZone() { result in
+		accountZone.fetchChangesInZone(incremental: false) { result in
 			switch result {
 			case .success:
 				
