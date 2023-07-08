7c726d837ec6bec279e750310364666d1524d433
Brent Simmons <brent@ranchero.com>
2023-07-08 12:09:30 -0700

    Use RSCore 2.0.1 or higher.

--- Articles/Package.swift
+++ Articles/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Articles"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMajor(from: "1.1.0")),
+		.package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMajor(from: "2.0.1")),
     ],
     targets: [
         .target(
