81f7ac147a418774dcc6e6ca3f4e97b05075bca4
Brent Simmons <brent@ranchero.com>
2023-07-14 11:18:04 -0700

    Replace webFeed with Feed in a few more places (which also fixes the add-feed sheet).

---
+++ Mac/MainWindow/AddFeed/AddFeedWindowControllerDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AddFeedWindowControllerDelegate.swift
+//  NetNewsWire
+//
+//  Created by Maurice Parker on 4/21/20.
+//  Copyright © 2020 Ranchero Software. All rights reserved.
+//
+
+import Foundation
+import Account
+
+@MainActor protocol AddFeedWindowControllerDelegate: AnyObject {
+
+	// userEnteredURL will have already been validated and normalized.
+	func addFeedWindowController(_: AddFeedWindowController, userEnteredURL: URL, userEnteredTitle: String?, container: Container)
+	func addFeedWindowControllerUserDidCancel(_: AddFeedWindowController)
+	
+}
+
+@MainActor protocol AddFeedWindowControllerProtocol {
+
+	var window: NSWindow? { get }
+	func runSheetOnWindow(_ hostWindow: NSWindow)
+	
+}
