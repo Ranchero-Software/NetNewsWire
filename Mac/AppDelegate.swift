071d02981886fa23353ef8da6c4d9693ef8ddbce
Brent Simmons <brent@ranchero.com>
2023-12-13 20:07:58 -0800

    Fix deprecation warning.

--- Mac/AppDelegate.swift
+++ Mac/AppDelegate.swift
@@ -481,7 +481,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserInterfaceValidations, 
 	// MARK: UNUserNotificationCenterDelegate
 	
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .badge, .sound])
+        completionHandler([.banner, .badge, .sound])
     }
 	
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
