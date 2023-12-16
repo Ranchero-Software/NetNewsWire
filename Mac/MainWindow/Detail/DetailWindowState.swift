434b495ffd11c4e9584394ba25279ff88b5bcd8e
Maurice Parker <mo@vincode.io>
2023-12-16 09:32:17 -0600

    Change Mac state preservation to be NSSecureCoding compliant

---
+++ Mac/MainWindow/Detail/DetailWindowState.swift
@@ -0,0 +1,9 @@
+//
+//  DetailWindowState.swift
+//  NetNewsWire
+//
+//  Created by Maurice Parker on 12/16/23.
+//  Copyright © 2023 Ranchero Software. All rights reserved.
+//
+
+import Foundation
---
+++ Mac/MainWindow/Timeline/TimelineWindowState.swift
@@ -0,0 +1,9 @@
+//
+//  TimelineWindowState.swift
+//  NetNewsWire
+//
+//  Created by Maurice Parker on 12/16/23.
+//  Copyright © 2023 Ranchero Software. All rights reserved.
+//
+
+import Foundation
