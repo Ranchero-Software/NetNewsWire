185ac2db1c9b81cec3ba556c8215c468726ff4c0
Brent Simmons <brent@ranchero.com>
2024-02-29 19:37:46 -0800

    Update Account/Package.swift

--- Account/Package.swift
+++ Account/Package.swift
@@ -1,39 +1,25 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
-
-var dependencies: [Package.Dependency] = [
-	.package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMinor(from: "1.0.0")),
-	.package(url: "https://github.com/Ranchero-Software/RSDatabase.git", .upToNextMajor(from: "1.0.0")),
-	.package(url: "https://github.com/Ranchero-Software/RSParser.git", .upToNextMajor(from: "2.0.2")),
-	.package(url: "https://github.com/Ranchero-Software/RSWeb.git", .upToNextMajor(from: "1.0.0")),
-]
-
-#if swift(>=5.6)
-dependencies.append(contentsOf: [
-	.package(path: "../Articles"),
-	.package(path: "../ArticlesDatabase"),
-	.package(path: "../Secrets"),
-	.package(path: "../SyncDatabase"),
-])
-#else
-dependencies.append(contentsOf: [
-	.package(url: "../Articles", .upToNextMajor(from: "1.0.0")),
-	.package(url: "../ArticlesDatabase", .upToNextMajor(from: "1.0.0")),
-	.package(url: "../Secrets", .upToNextMajor(from: "1.0.0")),
-	.package(url: "../SyncDatabase", .upToNextMajor(from: "1.0.0")),
-])
-#endif
 
 let package = Package(
     name: "Account",
-	platforms: [.macOS(SupportedPlatform.MacOSVersion.v10_15), .iOS(SupportedPlatform.IOSVersion.v13)],
+	platforms: [.macOS(.v14), .iOS(.v17)],
     products: [
         .library(
             name: "Account",
 			type: .dynamic,
             targets: ["Account"]),
     ],
-    dependencies: dependencies,
+    dependencies: [
+		.package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMinor(from: "1.0.0")),
+		.package(url: "https://github.com/Ranchero-Software/RSDatabase.git", .upToNextMajor(from: "1.0.0")),
+		.package(url: "https://github.com/Ranchero-Software/RSParser.git", .upToNextMajor(from: "2.0.2")),
+		.package(url: "https://github.com/Ranchero-Software/RSWeb.git", .upToNextMajor(from: "1.0.0")),
+		.package(path: "../Articles"),
+		.package(path: "../ArticlesDatabase"),
+		.package(path: "../Secrets"),
+		.package(path: "../SyncDatabase")
+	],
     targets: [
         .target(
             name: "Account",
