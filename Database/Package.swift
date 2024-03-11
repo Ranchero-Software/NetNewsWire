f2fea32111f69a3591f3834f207b0d4e3e18732d
Brent Simmons <brent@ranchero.com>
2024-03-10 18:24:14 -0700

    Add swiftSettings for strict concurrency but comment it out for now.

--- Database/Package.swift
+++ Database/Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -16,16 +15,17 @@ let package = Package(
 		.package(path: "../FMDB"),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
 			name: "Database",
 			dependencies: [
 				"FMDB"
 			]
+//			swiftSettings: [
+//				.enableExperimentalFeature("StrictConcurrency")
+//			]
 		),
 		.testTarget(
-            name: "DatabaseTests",
-            dependencies: ["Database"]),
-    ]
+			name: "DatabaseTests",
+			dependencies: ["Database"]),
+	]
 )
