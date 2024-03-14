273709115169ed2edb05504c27a979c3b03021c2
Brent Simmons <brent@ranchero.com>
2024-03-13 20:16:09 -0700

    Simplify Package.swift.

--- ArticlesDatabase/Package.swift
+++ ArticlesDatabase/Package.swift
@@ -1,24 +1,6 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
-var dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMinor(from: "1.0.0")),
-    .package(url: "https://github.com/Ranchero-Software/RSParser.git", .upToNextMajor(from: "2.0.2")),
-]
-
-#if swift(>=5.6)
-dependencies.append(contentsOf: [
-	.package(path: "../Articles"),
-	.package(path: "../Database"),
-	.package(path: "../FMDB"),
-])
-#else
-dependencies.append(contentsOf: [
-    .package(url: "../Articles", .upToNextMajor(from: "1.0.0")),
-])
-#endif
 
 let package = Package(
     name: "ArticlesDatabase",
@@ -29,7 +11,13 @@ let package = Package(
 			type: .dynamic,
             targets: ["ArticlesDatabase"]),
     ],
-    dependencies: dependencies,
+    dependencies: [
+		.package(url: "https://github.com/Ranchero-Software/RSCore.git", .upToNextMinor(from: "1.0.0")),
+		.package(url: "https://github.com/Ranchero-Software/RSParser.git", .upToNextMajor(from: "2.0.2")),
+		.package(path: "../Articles"),
+		.package(path: "../Database"),
+		.package(path: "../FMDB"),
+	],
     targets: [
         .target(
             name: "ArticlesDatabase",
