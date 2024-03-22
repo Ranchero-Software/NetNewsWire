9759d7dcd58b75aaa742af5f00604fa153b53d81
Brent Simmons <brent@ranchero.com>
2024-03-21 17:18:01 -0700

    Make RSTree a local package. Fix concurrency warnings in the package.

---
+++ Tree/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+	name: "Tree",
+	products: [
+		.library(
+			name: "Tree",
+			targets: ["Tree"]),
+	],
+	targets: [
+		.target(
+			name: "Tree",
+			dependencies: [],
+			swiftSettings: [
+				.enableExperimentalFeature("StrictConcurrency")
+			]
+		)
+	]
+)
