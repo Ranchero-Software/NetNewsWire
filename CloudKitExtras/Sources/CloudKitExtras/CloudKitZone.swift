80f34c1607f599a7c35da4987b3049bdd3546549
Brent Simmons <brent@ranchero.com>
2024-04-13 11:52:01 -0700

    Fix a couple typos in localized string.

--- CloudKitExtras/Sources/CloudKitExtras/CloudKitZone.swift
+++ CloudKitExtras/Sources/CloudKitExtras/CloudKitZone.swift
@@ -18,7 +18,7 @@ public enum CloudKitZoneError: LocalizedError {
 	public var errorDescription: String? {
 		switch self {
 		case .userDeletedZone:
-			return NSLocalizedString("The iCloud data was deleted.  Please remove the application iCloud account and add it again to continue using the application's iCloud support.", comment: "User deleted zone.")
+			return NSLocalizedString("The iCloud data was deleted. Please remove the application iCloud account and add it again to continue using the application’s iCloud support.", comment: "User deleted zone.")
 		case .corruptAccount:
 			return NSLocalizedString("There is an unrecoverable problem with your application iCloud account. Please make sure you have iCloud and iCloud Drive enabled in System Preferences. Then remove the application iCloud account and add it again.", comment: "Corrupt account.")
 		default:
