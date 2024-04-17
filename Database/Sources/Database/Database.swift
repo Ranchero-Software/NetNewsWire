f449a443d8e03bf4d18cfecc1616842b449b5662
Brent Simmons <brent@ranchero.com>
2024-04-16 21:49:36 -0700

    Remove unused code from Database module.

--- Database/Sources/Database/Database.swift
+++ Database/Sources/Database/Database.swift
@@ -11,14 +11,3 @@ import Foundation
 public enum DatabaseError: Error, Sendable {
 	case suspended // On iOS, to support background refreshing, a database may be suspended.
 }
-
-// Compatibility — to be removed once we switch to structured concurrency
-
-/// Completion block that provides an optional DatabaseError.
-public typealias DatabaseCompletionBlock = @Sendable (DatabaseError?) -> Void
-
-/// Result type for fetching an Int or getting a DatabaseError.
-public typealias DatabaseIntResult = Result<Int, DatabaseError>
-
-/// Completion block for DatabaseIntResult.
-public typealias DatabaseIntCompletionBlock = @Sendable (DatabaseIntResult) -> Void
