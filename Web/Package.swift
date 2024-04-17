ebe5e5fde7e6b7283b43f4e57315c616a7b592e8
Brent Simmons <brent@ranchero.com>
2024-04-16 21:47:29 -0700

    Remove unneeded comments.

--- Web/Package.swift
+++ Web/Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,14 +6,11 @@ let package = Package(
 	name: "Web",
 	platforms: [.macOS(.v14), .iOS(.v17)],
 	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
 			name: "Web",
 			targets: ["Web"]),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
 			name: "Web",
 			dependencies: [],
