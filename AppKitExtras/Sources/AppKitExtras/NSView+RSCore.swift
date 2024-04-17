814097504c0bc5c29e5618515d905965a20181e3
Brent Simmons <brent@ranchero.com>
2024-04-16 19:37:43 -0700

    Move constraintsToMakeSubViewFullSize into Core module.

--- AppKitExtras/Sources/AppKitExtras/NSView+RSCore.swift
+++ AppKitExtras/Sources/AppKitExtras/NSView+RSCore.swift
@@ -9,19 +9,25 @@
 import AppKit
 import FoundationExtras
 
-extension NSView {
-	
-    public func asImage() -> NSImage {
+public extension NSView {
+
+	func asImage() -> NSImage {
 		let rep = bitmapImageRepForCachingDisplay(in: bounds)!
 		cacheDisplay(in: bounds, to: rep)
-		
+
 		let img = NSImage(size: bounds.size)
 		img.addRepresentation(rep)
 		return img
-    }
-}
+	}
 
-public extension NSView {
+	func constraintsToMakeSubViewFullSize(_ subview: NSView) -> [NSLayoutConstraint] {
+
+		let leadingConstraint = NSLayoutConstraint(item: subview, attribute: .leading, relatedBy: .equal, toItem: self.safeAreaLayoutGuide, attribute: .leading, multiplier: 1.0, constant: 0.0)
+		let trailingConstraint = NSLayoutConstraint(item: subview, attribute: .trailing, relatedBy: .equal, toItem: self.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1.0, constant: 0.0)
+		let topConstraint = NSLayoutConstraint(item: subview, attribute: .top, relatedBy: .equal, toItem: self.safeAreaLayoutGuide, attribute: .top, multiplier: 1.0, constant: 0.0)
+		let bottomConstraint = NSLayoutConstraint(item: subview, attribute: .bottom, relatedBy: .equal, toItem: self.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+		return [leadingConstraint, trailingConstraint, topConstraint, bottomConstraint]
+	}
 
 	/// Keeps a subview at same size as receiver.
 	///
