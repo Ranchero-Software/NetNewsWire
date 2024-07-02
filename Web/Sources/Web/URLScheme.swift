cc71152b1689c83e6d8b5e99e9f2fcfb23377742
Brent Simmons <brent@ranchero.com>
2024-07-01 22:19:46 -0700

    Fix URLScheme — make members static.

--- Web/Sources/Web/URLScheme.swift
+++ Web/Sources/Web/URLScheme.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  URLScheme.swift
+//
 //
 //  Created by Brent Simmons on 6/30/24.
 //
@@ -9,8 +9,8 @@ import Foundation
 
 public struct URLScheme {
 
-	public let http = "http"
-	public let https = "https"
-	public let mailto = "mailto"
-	public let tel = "tel"
+	public static let http = "http"
+	public static let https = "https"
+	public static let mailto = "mailto"
+	public static let tel = "tel"
 }
