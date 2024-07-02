36982adb7c702aca5647014528da7c20dbd48981
Brent Simmons <brent@ranchero.com>
2024-07-01 21:47:21 -0700

    Add URLScheme constants to Web module.

---
+++ Web/Sources/Web/URLScheme.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brent Simmons on 6/30/24.
+//
+
+import Foundation
+
+public struct URLScheme {
+
+	public let http = "http"
+	public let https = "https"
+	public let mailto = "mailto"
+	public let tel = "tel"
+}
