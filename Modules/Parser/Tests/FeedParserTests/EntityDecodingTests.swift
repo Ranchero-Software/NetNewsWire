a48615b06063c36687f99007b74cf02732f99fa7
Brent Simmons <brent@ranchero.com>
2024-09-16 22:07:31 -0700

    Add additional EntityDecodingTests.

--- Modules/Parser/Tests/FeedParserTests/EntityDecodingTests.swift
+++ Modules/Parser/Tests/FeedParserTests/EntityDecodingTests.swift
@@ -21,7 +21,39 @@ class EntityDecodingTests: XCTestCase {
 		XCTAssertEqual(decoded, "These are the times that try men's souls.")
 	}
 
-	func testEntities() {
+	func testEntityAtBeginning() {
+
+		let s = "&#39;leading single quote"
+		let decoded = HTMLEntityDecoder.decodedString(s)
+
+		XCTAssertEqual(decoded, "'leading single quote")
+	}
+
+	func testEntityAtEnd() {
+
+		let s = "trailing single quote&#39;"
+		let decoded = HTMLEntityDecoder.decodedString(s)
+
+		XCTAssertEqual(decoded, "trailing single quote'")
+	}
+
+	func testEntityInMiddle() {
+
+		let s = "entity &ccedil; in middle"
+		let decoded = HTMLEntityDecoder.decodedString(s)
+
+		XCTAssertEqual(decoded, "entity ç in middle")
+	}
+
+	func testMultipleEntitiesInARow() {
+
+		let s = "&ccedil;&egrave;mult&#8230;&#x2026;iple &#39;&aelig;&quot;entities&divide;&hearts;"
+		let decoded = HTMLEntityDecoder.decodedString(s)
+
+		XCTAssertEqual(decoded, "çèmult……iple 'æ\"entities÷♥")
+	}
+
+	func testOnlyEntity() {
 		var s = "&#8230;"
 		var decoded = HTMLEntityDecoder.decodedString(s)
 
