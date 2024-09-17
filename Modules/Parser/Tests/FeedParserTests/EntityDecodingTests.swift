88675adff2c5916c2c92f92c7f0dd24ebfb83f17
Brent Simmons <brent@ranchero.com>
2024-09-16 21:57:05 -0700

    Restore EntityDecodingTests.

--- Modules/Parser/Tests/FeedParserTests/EntityDecodingTests.swift
+++ Modules/Parser/Tests/FeedParserTests/EntityDecodingTests.swift
@@ -7,41 +7,40 @@
 //
 
 import XCTest
-import FeedParser
+import SAX
 
-//class EntityDecodingTests: XCTestCase {
-//
-//    func test39Decoding() {
-//
-//		// Bug found by Manton Reece — the &#39; entity was not getting decoded by NetNewsWire in JSON Feeds from micro.blog.
-//
-//		let s = "These are the times that try men&#39;s souls."
-//		let decoded = s.rsparser_stringByDecodingHTMLEntities()
-//
-//		XCTAssertEqual(decoded, "These are the times that try men's souls.")
-//	}
-//
-//	func testEntities() {
-//		var s = "&#8230;"
-//		var decoded = s.rsparser_stringByDecodingHTMLEntities()
-//
-//		XCTAssertEqual(decoded, "…")
-//
-//		s = "&#x2026;"
-//		decoded = s.rsparser_stringByDecodingHTMLEntities()
-//		XCTAssertEqual(decoded, "…")
-//
-//		s = "&#039;"
-//		decoded = s.rsparser_stringByDecodingHTMLEntities()
-//		XCTAssertEqual(decoded, "'")
-//
-//		s = "&#167;"
-//		decoded = s.rsparser_stringByDecodingHTMLEntities()
-//		XCTAssertEqual(decoded, "§")
-//
-//		s = "&#XA3;"
-//		decoded = s.rsparser_stringByDecodingHTMLEntities()
-//		XCTAssertEqual(decoded, "£")
-//
-//	}
-//}
+class EntityDecodingTests: XCTestCase {
+
+    func test39Decoding() {
+
+		// Bug found by Manton Reece — the &#39; entity was not getting decoded by NetNewsWire in JSON Feeds from micro.blog.
+
+		let s = "These are the times that try men&#39;s souls."
+		let decoded = HTMLEntityDecoder.decodedString(s)
+
+		XCTAssertEqual(decoded, "These are the times that try men's souls.")
+	}
+
+	func testEntities() {
+		var s = "&#8230;"
+		var decoded = HTMLEntityDecoder.decodedString(s)
+
+		XCTAssertEqual(decoded, "…")
+
+		s = "&#x2026;"
+		decoded = HTMLEntityDecoder.decodedString(s)
+		XCTAssertEqual(decoded, "…")
+
+		s = "&#039;"
+		decoded = HTMLEntityDecoder.decodedString(s)
+		XCTAssertEqual(decoded, "'")
+
+		s = "&#167;"
+		decoded = HTMLEntityDecoder.decodedString(s)
+		XCTAssertEqual(decoded, "§")
+
+		s = "&#XA3;"
+		decoded = HTMLEntityDecoder.decodedString(s)
+		XCTAssertEqual(decoded, "£")
+	}
+}
