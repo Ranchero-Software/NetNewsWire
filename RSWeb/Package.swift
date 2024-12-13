c38e5d6abe52e100007ab484b874cd698289c55a
Brent Simmons <brent@ranchero.com>
2024-12-13 08:57:31 -0800

    Add HTMLMetadataCache to RSWeb.

--- RSWeb/Package.swift
+++ RSWeb/Package.swift
@@ -3,23 +3,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "RSWeb",
+	name: "RSWeb",
 	platforms: [.macOS(.v13), .iOS(.v16)],
 	products: [
-        .library(
-            name: "RSWeb",
-            type: .dynamic,
-            targets: ["RSWeb"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(
-            name: "RSWeb",
+		.library(
+			name: "RSWeb",
+			type: .dynamic,
+			targets: ["RSWeb"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/Ranchero-Software/RSParser.git", .upToNextMinor(from: "2.0.2")),
+		.package(path: "../Core")
+	],
+	targets: [
+		.target(
+			name: "RSWeb",
+			dependencies: [
+				"RSParser",
+				"Core"
+			],
 			resources: [.copy("UTS46/uts46")],
 			swiftSettings: [.define("SWIFT_PACKAGE")]),
-        .testTarget(
-            name: "RSWebTests",
-            dependencies: ["RSWeb"]),
-    ]
+		.testTarget(
+			name: "RSWebTests",
+			dependencies: [
+				"RSWeb",
+				"RSParser",
+				"Core"
+			]),
+	]
 )
