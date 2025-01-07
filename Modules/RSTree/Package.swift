3d4f5271c130c66b223cb68b7a88a972fdd2035e
Brent Simmons <brent@ranchero.com>
2025-01-06 22:01:11 -0800

    Add platform to RSTree.

--- Modules/RSTree/Package.swift
+++ Modules/RSTree/Package.swift
@@ -1,8 +1,10 @@
 // swift-tools-version:5.10
+
 import PackageDescription
 
 let package = Package(
 	name: "RSTree",
+	platforms: [.macOS(.v14), .iOS(.v17)],
 	products: [
 		.library(
 			name: "RSTree",
