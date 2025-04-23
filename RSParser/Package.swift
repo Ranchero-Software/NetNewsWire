026a63be7fb1323d12975fc7fa4e64618b7b31c2
Brent Simmons <brent@ranchero.com>
2025-04-22 21:51:28 -0700

    Regularize RSParser Package.swift. Fix #4605.

--- RSParser/Package.swift
+++ RSParser/Package.swift
@@ -1,32 +1,26 @@
 // swift-tools-version:5.10
-
 import PackageDescription
 
 let package = Package(
-    name: "RSParser",
+	name: "RSParser",
 	platforms: [.macOS(.v13), .iOS(.v17)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "RSParser",
-            type: .dynamic,
-            targets: ["RSParser"]),
-        .library(
-            name: "RSParserObjC",
-            type: .dynamic,
-            targets: ["RSParserObjC"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "RSParser",
-            dependencies: ["RSParserObjC"],
-            path: "Sources/Swift"),
+	products: [
+		.library(
+			name: "RSParser",
+			type: .dynamic,
+			targets: ["RSParser"]),
+		.library(
+			name: "RSParserObjC",
+			type: .dynamic,
+			targets: ["RSParserObjC"]),
+	],
+	dependencies: [
+	],
+	targets: [
+		.target(
+			name: "RSParser",
+			dependencies: ["RSParserObjC"],
+			path: "Sources/Swift"),
 		.target(
 			name: "RSParserObjC",
 			dependencies: [],
@@ -39,10 +33,10 @@ let package = Package(
 				.unsafeFlags(["-fprofile-instr-generate"])
 			]
 		),
-        .testTarget(
-            name: "RSParserTests",
-            dependencies: ["RSParser"],
-            exclude: ["Info.plist"],
-            resources: [.copy("Resources")]),
-    ]
+		.testTarget(
+			name: "RSParserTests",
+			dependencies: ["RSParser"],
+			exclude: ["Info.plist"],
+			resources: [.copy("Resources")]),
+	]
 )
