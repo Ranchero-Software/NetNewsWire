b28e9c6bf76d705281bc5b277269a8fe08dec9ca
Brent Simmons <brent@ranchero.com>
2025-04-22 21:50:13 -0700

    Regularize SyncDatabase Package.swift. Fix #4604.

--- SyncDatabase/Package.swift
+++ SyncDatabase/Package.swift
@@ -1,38 +1,27 @@
 // swift-tools-version:5.10
 import PackageDescription
 
-var dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/Ranchero-Software/RSDatabase.git", .upToNextMajor(from: "1.0.0")),
-]
-
-#if swift(>=5.6)
-dependencies.append(contentsOf: [
-	.package(path: "../Articles"),
-	.package(path: "../RSCore"),
-])
-#else
-dependencies.append(contentsOf: [
-    .package(url: "../Articles", .upToNextMajor(from: "1.0.0")),
-])
-#endif
-
 let package = Package(
-    name: "SyncDatabase",
+	name: "SyncDatabase",
 	platforms: [.macOS(.v13), .iOS(.v17)],
-    products: [
-        .library(
-            name: "SyncDatabase",
+	products: [
+		.library(
+			name: "SyncDatabase",
 			type: .dynamic,
-            targets: ["SyncDatabase"]),
-    ],
-    dependencies: dependencies,
-    targets: [
-        .target(
-            name: "SyncDatabase",
-            dependencies: [
+			targets: ["SyncDatabase"]),
+	],
+	dependencies: [
+		.package(path: "../Articles"),
+		.package(path: "../RSCore"),
+		.package(path: "../RSDatabase"),
+	],
+	targets: [
+		.target(
+			name: "SyncDatabase",
+			dependencies: [
 				"RSCore",
 				"RSDatabase",
 				"Articles",
 			]),
-    ]
+	]
 )
