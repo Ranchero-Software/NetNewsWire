4bb71db84609630794c3a9e365d2a9942f57e204
Brent Simmons <brent@ranchero.com>
2025-04-24 08:58:57 -0700

    Move gyb to buildscripts. Delete Vendor folder.

--- buildscripts/updateSecrets.sh
+++ buildscripts/updateSecrets.sh
@@ -3,5 +3,5 @@
 find "${PROJECT_DIR}" -name '*.gyb' |
   while read file; do
     echo "Generating ${file%.gyb}";
-    "${PROJECT_DIR}/Vendor/gyb" --line-directive '' -o "${file%.gyb}" "$file";
+    "${PROJECT_DIR}/buildscripts/gyb" --line-directive '' -o "${file%.gyb}" "$file";
   done
