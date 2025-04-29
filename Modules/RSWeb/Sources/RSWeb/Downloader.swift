2658319a912be8587753c9c6631c1993634222d2
Brent Simmons <brent@ranchero.com>
2025-04-29 13:09:10 -0700

    Make Download @MainActor, and make it handle duplicate in-flight requests without making duplicate calls.

--- Modules/RSWeb/Sources/RSWeb/Downloader.swift
+++ Modules/RSWeb/Sources/RSWeb/Downloader.swift
@@ -7,15 +7,20 @@
 //
 
 import Foundation
+import os
 
-public typealias DownloadCallback = (Data?, URLResponse?, Error?) -> Swift.Void
+public typealias DownloadCallback = @MainActor (Data?, URLResponse?, Error?) -> Swift.Void
 
 /// Simple downloader, for a one-shot download like an image
 /// or a web page. For a download-feeds session, see DownloadSession.
-public final class Downloader {
+@MainActor public final class Downloader {
 
 	public static let shared = Downloader()
 	private let urlSession: URLSession
+	private var callbacks = [URL: [DownloadCallback]]()
+	
+	private static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Downloader")
+	private static let debugLoggingEnabled = true
 
 	private init() {
 
@@ -37,18 +42,60 @@ public final class Downloader {
 		urlSession.invalidateAndCancel()
 	}
 
-	public func download(_ url: URL, _ completion: DownloadCallback? = nil) {
+	public func download(_ url: URL, _ completion: @escaping DownloadCallback) {
 		download(URLRequest(url: url), completion)
 	}
 
-	public func download(_ urlRequest: URLRequest, _ completion: DownloadCallback? = nil) {
+	public func download(_ urlRequest: URLRequest, _ completion: @escaping DownloadCallback) {
+		assert(Thread.isMainThread)
+
+		guard let url = urlRequest.url else {
+			Self.logger.fault("Downloader: skipping download for URLRequest without a URL")
+			return
+		}
+
+		if callbacks[url] == nil {
+			if Self.debugLoggingEnabled {
+				Self.logger.debug("Downloader: downloading \(url)")
+			}
+			callbacks[url] = [completion]
+		} else {
+			// A download is already be in progress for this URL. Don’t start a separate download.
+			// Add the callback to the callbacks array for this URL.
+			if Self.debugLoggingEnabled {
+				Self.logger.debug("Downloader: download in progress for \(url) — adding callback")
+			}
+			callbacks[url]?.append(completion)
+			return
+		}
 
 		var urlRequestToUse = urlRequest
 		urlRequestToUse.addSpecialCaseUserAgentIfNeeded()
 
 		let task = urlSession.dataTask(with: urlRequestToUse) { (data, response, error) in
-			DispatchQueue.main.async() {
-				completion?(data, response, error)
+			Task { @MainActor in
+				let callbacksForURL = self.callbacks[url]
+				assert(callbacksForURL != nil)
+
+				if let callbacksForURL {
+					if Self.debugLoggingEnabled {
+						let count = callbacksForURL.count
+						if count == 1 {
+							Self.logger.debug("Downloader: calling 1 callback for URL \(url)")
+						} else {
+							Self.logger.debug("Downloader: calling \(callbacksForURL.count) callbacks for URL \(url)")
+						}
+					}
+
+					for completion in callbacksForURL {
+						completion(data, response, error)
+					}
+				} else {
+					assertionFailure("Downloader: downloaded URL \(url) but no callbacks found")
+					Self.logger.fault("Downloader: downloaded URL \(url) but no callbacks found")
+				}
+
+				self.callbacks[url] = nil
 			}
 		}
 		task.resume()
