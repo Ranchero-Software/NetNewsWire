0483e9bb9db5dec4ee9b4d92fa69005be7d773a2
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2025-06-20 19:50:52 +0800

    🔥 Don’t use image there!

--- Mac/MainWindow/AddFeed/FolderTreeMenu.swift
+++ Mac/MainWindow/AddFeed/FolderTreeMenu.swift
@@ -29,7 +29,6 @@ class FolderTreeMenu {
 			}
 			
 			let menuItem = NSMenuItem(title: account.nameForDisplay, action: nil, keyEquivalent: "")
-			menuItem.image = AppAssets.image(for: <#T##AccountType#>)
 			menuItem.representedObject = childNode.representedObject
 			
 			if account.behaviors.contains(.disallowFeedInRootFolder) {
