4ba2bc73832632339f91e8691914d545f95c5882
Brent Simmons <brent@ranchero.com>
2025-09-22 18:09:43 -0700

    Add error recovery to UIViewController.presentError extension. This makes it work more like the Mac version.

--- iOS/UIKit Extensions/UIViewController-Extensions.swift
+++ iOS/UIKit Extensions/UIViewController-Extensions.swift
@@ -47,8 +47,14 @@ extension UIViewController {
 				presentError(title: errorTitle, message: informativeText, dismiss: dismiss)
 			}
 		} else {
-			let errorTitle = NSLocalizedString("Error", comment: "Error")
-			presentError(title: errorTitle, message: error.localizedDescription, dismiss: dismiss)
+			// Check if error supports recovery options
+			if let recoverableError = error as? (RecoverableError & LocalizedError),
+			   !recoverableError.recoveryOptions.isEmpty {
+				presentErrorWithRecovery(error: recoverableError, dismiss: dismiss)
+			} else {
+				let errorTitle = NSLocalizedString("Error", comment: "Error")
+				presentError(title: errorTitle, message: error.localizedDescription, dismiss: dismiss)
+			}
 		}
 	}
 
@@ -83,6 +89,29 @@ private extension UIViewController {
 			dismiss?()
 		}
 		alertController.addAction(dismissAction)
+		
+		self.present(alertController, animated: true, completion: nil)
+	}
+	
+	func presentErrorWithRecovery(error: RecoverableError & LocalizedError, dismiss: (() -> Void)? = nil) {
+		let title = error.errorDescription ?? NSLocalizedString("Error", comment: "Error")
+		let message = [error.failureReason, error.recoverySuggestion].compactMap { $0 }.joined(separator: " ")
+		
+		let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		
+		// Add recovery options as buttons
+		for (index, option) in error.recoveryOptions.enumerated() {
+			let action = UIAlertAction(title: option, style: index == 0 ? .default : .cancel) { _ in
+				dismiss?()
+				_ = error.attemptRecovery(optionIndex: index)
+			}
+			alertController.addAction(action)
+			
+			// Make the first option the preferred action
+			if index == 0 {
+				alertController.preferredAction = action
+			}
+		}
 		
 		self.present(alertController, animated: true, completion: nil)
 	}
