4bdca9d81bc24124fd2f4135c28b754e596a8cef
Brent Simmons <brent@ranchero.com>
2025-10-02 09:36:38 -0700

    Create common Logger for Feedly code.

---
+++ Modules/Account/Sources/Account/Feedly/Feedly.swift
@@ -0,0 +1,13 @@
+//
+//  Feedly.swift
+//  Account
+//
+//  Created by Brent Simmons on 10/2/25.
+//
+
+import Foundation
+
+struct Feedly {
+
+	static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Feedly")
+}
