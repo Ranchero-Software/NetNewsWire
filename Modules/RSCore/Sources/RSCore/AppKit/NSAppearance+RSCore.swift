8a3b4e393a734f8847b738ad03c91f24f9073198
Brent Simmons <brent@ranchero.com>
2025-11-19 15:27:42 -0800

    Move CGImage extension to RSCore.

--- Modules/RSCore/Sources/RSCore/AppKit/NSAppearance+RSCore.swift
+++ Modules/RSCore/Sources/RSCore/AppKit/NSAppearance+RSCore.swift
@@ -10,15 +10,8 @@
 import AppKit
 
 extension NSAppearance {
-
-	@objc(rsIsDarkMode)
 	@MainActor public var isDarkMode: Bool {
-		if #available(macOS 10.14, *) {
-			return self.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
-		}
-		else {
-			return false
-		}
+		bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
 	}
 }
 #endif
