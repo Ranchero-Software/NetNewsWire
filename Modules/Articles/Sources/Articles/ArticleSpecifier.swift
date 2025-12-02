3ecf9887a86f22a1ac6e614b0b433b6a76cb6e15
Brent Simmons <brent@ranchero.com>
2025-12-01 21:16:47 -0800

    Add ArticleSpecifier — useful to save to disk.

---
+++ Modules/Articles/Sources/Articles/ArticleSpecifier.swift
@@ -0,0 +1,43 @@
+//
+//  ArticleSpecifier.swift
+//  Articles
+//
+//  Created by Brent Simmons on 12/1/25.
+//
+
+import Foundation
+
+/// Use this to refer to an Article when you can look it up.
+///
+/// For instance, this can be saved to disk as part of state restoration.
+struct ArticleSpecifier: Hashable, Sendable {
+	let accountID: String
+	let articleID: String
+
+	private struct Key {
+		static let accountID = "accountID"
+		static let articleID = "articleID"
+	}
+
+	/// Plist-compatible dictionary, able to saved to disk or in UserDefaults.
+	var dictionary: [String: String] {
+		[Key.accountID: accountID, Key.articleID: articleID]
+	}
+
+	init(accountID: String, articleID: String) {
+		self.accountID = accountID
+		self.articleID = articleID
+	}
+
+	init?(dictionary: [String: String]) {
+		guard let accountID = dictionary[Key.accountID],
+			  let articleID = dictionary[Key.articleID] else {
+			return nil
+		}
+		self.init(accountID: accountID, articleID: articleID)
+	}
+
+	init(article: Article) {
+		self.init(accountID: article.accountID, articleID: article.articleID)
+	}
+}
