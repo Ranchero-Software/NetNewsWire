a7c7412dfc7620bfa408c0a43d2fe92b93afe0a1
Brent Simmons <brent@ranchero.com>
2025-12-30 21:26:34 -0800

    Split FetchRequestOperation (temporarily) until we start using HidingReadArticlesState on Mac.

--- Shared/Timeline/FetchRequestOperation.swift
+++ Shared/Timeline/FetchRequestOperation.swift
@@ -16,6 +16,12 @@ import Articles
 
 typealias FetchRequestOperationResultBlock = (Set<Article>, FetchRequestOperation) -> Void
 
+// TODO: unify these two versions of FetchRequestOperation,
+// which diverged when we changed — just on iOS — how we keep track
+// of sidebar item hide-read-articles settings.
+
+#if os(macOS)
+
 @MainActor final class FetchRequestOperation {
 
 	let id: Int
@@ -96,3 +102,93 @@ typealias FetchRequestOperationResultBlock = (Set<Article>, FetchRequestOperatio
 	}
 }
 
+#else
+
+@MainActor final class FetchRequestOperation {
+
+	let id: Int
+	let hidingReadArticlesState: HidingReadArticlesState
+	let resultBlock: FetchRequestOperationResultBlock
+	var isCanceled = false
+	var isFinished = false
+	private let fetchers: [ArticleFetcher]
+
+	init(id: Int, hidingReadArticlesState: HidingReadArticlesState, fetchers: [ArticleFetcher], resultBlock: @escaping FetchRequestOperationResultBlock) {
+		precondition(Thread.isMainThread)
+		self.id = id
+		self.hidingReadArticlesState = hidingReadArticlesState
+		self.fetchers = fetchers
+		self.resultBlock = resultBlock
+	}
+
+	@MainActor func run(_ completion: @escaping (FetchRequestOperation) -> Void) {
+		precondition(Thread.isMainThread)
+		precondition(!isFinished)
+
+		Task { @MainActor in
+			var didCallCompletion = false
+
+			func callCompletionIfNeeded() {
+				if !didCallCompletion {
+					didCallCompletion = true
+					completion(self)
+				}
+			}
+
+			if isCanceled {
+				callCompletionIfNeeded()
+				return
+			}
+
+			if fetchers.isEmpty {
+				isFinished = true
+				resultBlock(Set<Article>(), self)
+				callCompletionIfNeeded()
+				return
+			}
+
+			let numberOfFetchers = fetchers.count
+			var fetchersReturned = 0
+			var fetchedArticles = Set<Article>()
+
+			@MainActor func process(_ articles: Set<Article>) {
+				precondition(Thread.isMainThread)
+				guard !self.isCanceled else {
+					callCompletionIfNeeded()
+					return
+				}
+
+				assert(!self.isFinished)
+
+				fetchedArticles.formUnion(articles)
+				fetchersReturned += 1
+				if fetchersReturned == numberOfFetchers {
+					self.isFinished = true
+					self.resultBlock(fetchedArticles, self)
+					callCompletionIfNeeded()
+				}
+			}
+
+			@MainActor func fetcherHidesReadArticles(_ fetcher: ArticleFetcher) -> Bool {
+				guard let feed = fetcher as? Feed, let sidebarItemID = feed.sidebarItemID else {
+					return false
+				}
+				return hidingReadArticlesState.isHiding(for: sidebarItemID)
+			}
+
+			for fetcher in fetchers {
+				if fetcherHidesReadArticles(fetcher) {
+					if let articles = try? await fetcher.fetchUnreadArticlesAsync() {
+						process(articles)
+					}
+				} else {
+					if let articles = try? await fetcher.fetchArticlesAsync() {
+						process(articles)
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
