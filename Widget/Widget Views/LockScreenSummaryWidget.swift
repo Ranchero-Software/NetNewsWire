cf103f013cd4b5525ce641a08652bf4af05dacf7
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2026-01-08 17:34:59 +0800

    💄 Update lock screen widget alignment

--- Widget/Widget Views/LockScreenSummaryWidget.swift
+++ Widget/Widget Views/LockScreenSummaryWidget.swift
@@ -15,51 +15,44 @@ struct LockScreenSummaryWidgetView: View {
 	
     var body: some View {
 		VStack(alignment: .leading, spacing: 2) {
-			HStack {
+			HStack(alignment: .center) {
 				unreadImage
 				Text("label.text.unread", comment: "Unread")
 				Spacer()
 				Text(verbatim: entry.widgetData.currentUnreadCount.formatted())
+					.frame(maxWidth: .infinity, alignment: .trailing)
 			}
-			HStack {
+			HStack(alignment: .center) {
 				todayImage
 				Text("label.text.today", comment: "Today")
 				Spacer()
 				Text(verbatim: entry.widgetData.currentTodayCount.formatted())
+					.frame(maxWidth: .infinity, alignment: .trailing)
 			}
-			HStack {
+			HStack(alignment: .center) {
 				starredImage
 				Text("label.text.starred", comment: "Starred")
 				Spacer()
 				Text(verbatim: entry.widgetData.currentStarredCount.formatted())
+					.frame(maxWidth: .infinity, alignment: .trailing)
 			}
 		}
 		.font(.caption)
-		.padding(2)
     }
 	
 	var starredImage: some View {
 		Image(systemName: "star.fill")
-			.resizable()
-			.frame(width: WidgetLayout.titleImageSize, height: WidgetLayout.titleImageSize, alignment: .top)
-			.cornerRadius(4)
-			.foregroundColor(.yellow)
+			.frame(width: 14, height: 14)
 	}
 	
 	var unreadImage: some View {
 		Image(systemName: "largecircle.fill.circle")
-			.resizable()
-			.frame(width: WidgetLayout.titleImageSize, height: WidgetLayout.titleImageSize, alignment: .top)
-			.foregroundColor(.accentColor)
+			.frame(width: 14, height: 14)
 	}
 	
 	var todayImage: some View {
 		Image(systemName: "sun.max.fill")
-			.resizable()
-			.frame(width: WidgetLayout.titleImageSize, height: WidgetLayout.titleImageSize, alignment: .top)
-			.cornerRadius(4)
-			.foregroundColor(.orange)
+			.frame(width: 14, height: 14)
 	}
-	
 	
 }
