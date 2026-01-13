6dbd521aafeef42cf9da9d71a5a31a285714687c
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2026-01-14 07:01:44 +0800

    🩹 Handle title-less articles in cell layout.
    Only add a new line between title and summary when a title exists.

--- iOS/MainTimeline/Cells/MainTimelineIconFeedCell.swift
+++ iOS/MainTimeline/Cells/MainTimelineIconFeedCell.swift
@@ -147,7 +147,12 @@ class MainTimelineIconFeedCell: UITableViewCell {
 					.paragraphStyle: paragraphStyle,
 					.foregroundColor: isSelected ? UIColor.white : UIColor.label
 				]
-				let summaryAttributed = NSAttributedString(string: "\n" + cellData.summary, attributes: summaryAttributes)
+				var summaryAttributed: NSAttributedString
+				if cellData.title != "" {
+					summaryAttributed = NSAttributedString(string: "\n" + cellData.summary, attributes: summaryAttributes)
+				} else {
+					summaryAttributed = NSAttributedString(string: cellData.summary, attributes: summaryAttributes)
+				}
 				attributedCellText.append(summaryAttributed)
 			}
 			articleTitle.attributedText = attributedCellText
