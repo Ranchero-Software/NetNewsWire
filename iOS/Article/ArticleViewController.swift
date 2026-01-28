8ec78a26bcc72d25d1d766b58f08ed4be61c5c69
Brent Simmons <brent@ranchero.com>
2026-01-27 19:29:11 -0800

    Use label color instead of accent color so that the button doesn’t appear blue. Fix #5036.

--- iOS/Article/ArticleViewController.swift
+++ iOS/Article/ArticleViewController.swift
@@ -41,6 +41,7 @@ final class ArticleViewController: UIViewController {
 		let button = ArticleExtractorButton(type: .system)
 		button.frame = CGRect(x: 0, y: 0, width: 44.0, height: 44.0)
 		button.setImage(Assets.Images.articleExtractorOff, for: .normal)
+		button.tintColor = .label
 		return button
 	}()
 
