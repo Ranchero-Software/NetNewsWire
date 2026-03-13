90d599ef8dace1ff58ec308bc29055f33eda56cf
Brent Simmons <brent@ranchero.com>
2026-03-12 19:41:06 -0700

    Add helper method for creating userInfo for error log notification.

--- Modules/ErrorLog/Sources/ErrorLog/ErrorLogNotification.swift
+++ Modules/ErrorLog/Sources/ErrorLog/ErrorLogNotification.swift
@@ -19,4 +19,12 @@ public struct ErrorLogUserInfoKey {
 	public static let sourceName = "sourceName"
 	public static let sourceID = "sourceID" // 0-99 are AccountType raw values. 100 and greater are for other components.
 	public static let errorMessage = "errorMessage"
+
+	public static func userInfo(sourceName: String, sourceID: Int, errorMessage: String) -> [String: Any] {
+		[
+			Self.sourceName: sourceName,
+			Self.sourceID: sourceID,
+			Self.errorMessage: errorMessage
+		]
+	}
 }
