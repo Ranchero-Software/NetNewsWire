5f6445c953ee15bbe28e95fb5f379a3b63382646
Brent Simmons <brent@ranchero.com>
2026-03-18 20:48:10 -0700

    Create AppDefaults+Shared. Create wrapper and migration for the new iCloudSyncArticleContentForUnreadArticles setting.

---
+++ Shared/AppDefaults+Shared.swift
@@ -0,0 +1,33 @@
+//
+//  AppDefaults+Shared.swift
+//  NetNewsWire
+//
+//  Created by Brent Simmons on 3/18/26.
+//  Copyright © 2026 Ranchero Software. All rights reserved.
+//
+
+import Foundation
+import Account
+
+extension AppDefaults {
+
+	static let iCloudSyncArticleContentForUnreadArticles = "iCloudSyncArticleContentForUnreadArticles"
+
+	var iCloudSyncArticleContentForUnreadArticles: Bool {
+		get {
+			UserDefaults.standard.bool(forKey: Self.iCloudSyncArticleContentForUnreadArticles)
+		}
+		set {
+			UserDefaults.standard.set(newValue, forKey: Self.iCloudSyncArticleContentForUnreadArticles)
+		}
+	}
+
+	func migrateiCloudSyncArticleContentForUnreadArticlesSetting() {
+		// iCloudSyncArticleContentForUnreadArticles should be set to false unless
+		// the user already has an iCloud account.
+		guard UserDefaults.standard.object(forKey: Self.iCloudSyncArticleContentForUnreadArticles) == nil else {
+			return
+		}
+		iCloudSyncArticleContentForUnreadArticles = AccountManager.shared.hasiCloudAccount
+	}
+}
