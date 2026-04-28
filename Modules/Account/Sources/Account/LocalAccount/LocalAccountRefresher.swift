5d5fcb13bbcf159f95524fbe794cf7af58955822
Brent Simmons <brent@ranchero.com>
2026-04-27 20:32:05 -0700

    Add additional no-minimum domains.

--- Modules/Account/Sources/Account/LocalAccount/LocalAccountRefresher.swift
+++ Modules/Account/Sources/Account/LocalAccount/LocalAccountRefresher.swift
@@ -256,7 +256,7 @@ private extension LocalAccountRefresher {
 		"daringfireball.net", "redsweater.com", "indiestack.com",
 		"blog.plunkitup.com", "bitsplitting.org", "allenpike.com",
 		"hypercritical.co", "micro.inessential.com", "discourse.netnewswire.com",
-		"onefoottsunami.com"
+		"onefoottsunami.com", "manton.org", "randsinrepose.com"
 	]
 
 	/// Return true if we won’t download that feed.
